212fda425da92ef3be9b1942be522c0e3ae6208e
Jon <jonathan.woolfrey@gmail.com>
2021-12-12 19:00:26 +0100

    Still in development - wanted to save my work

--- Trajectory/CartesianTrajectory.h
+++ Trajectory/CartesianTrajectory.h
@@ -31,15 +31,96 @@ class CartesianTrajectory
 		Quintic quinticPosition;
 		Quintic quinticOrientation;
 		
+		// These variables are used to combine position & orientation
+		Eigen::VectorXf pos, linearVel, linearAcc;
+		Eigen::Vector3f angularVel, angularAcc;
+		Eigen::Quaternionf quat;
+		
 };										// Semicolon needed after a class declaration
 
 CartesianTrajectory::CartesianTrajectory(const std::vector<Eigen::Affine3f> &poses,
 					const std::vector<float> &times)
+					:
+					n(poses.size()),
+					pos(Eigen::VectorXf(3)),
+					linearVel(Eigen::VectorXf(3)),
+					linearAcc(Eigen::VectorXf(3))
 {
-	// Worker bees can leave.
-	// Even drones can fly away.
-	// The Queen is their slave.
+	// Check the inputs are sound
+	if(poses.size() != times.size())
+	{
+		std::cout << "ERROR: CartesianTrajectory::CartesianTrajectory() : Input vectors are not of equal length!" << std::endl;
+	}
+	
+	// Create minimum jerk trajectory
+	if(this->n == 2)
+	{
+		this->quinticPosition = Quintic(poses[0].translation(),
+						poses[1].translation(),
+						times[0],
+						times[1]);
+						
+		this->quinticOrientation = Quintic(Eigen::Quaternionf(poses[0].linear()),
+						    Eigen::Quaternionf(poses[1].linear()),
+						    times[0],
+						    times[1]);
+	}
+	
+	// Create minimum acceleration trajectory
+	else if(this->n > 2)
+	{
+		std::vector<Eigen::VectorXf> position;
+		std::vector<Eigen::Quaternionf> orientation;
+		
+		for(int i = 0; i < this->n; i++)
+		{
+			position.push_back(poses[i].translation());
+			orientation.push_back(Eigen::Quaternionf(poses[i].linear()));
+		}
+		
+		this->cubicPosition = CubicSpline(position, times);
+		this->cubicOrientation = CubicSpline(orientation, times);
+	}
+	else std::cout << "ERROR: CartesianTrajectory::CartesianTrajectory() : At least 2 poses are needed to generate a trajectory!" << std::endl;
 }
+
+void CartesianTrajectory::get_state(Eigen::Affine3f &pose,
+				Eigen::VectorXf &vel,
+				Eigen::VectorXf &acc,
+				const float &time)
+{
+	if(vel.size() != 6 || acc.size() != 6)
+	{
+		std::cout << "ERROR: CartesianTrajectory::get_state() : Velocity and acceleration vectors must have 6 elements!" << std::endl;
+	}
+	else
+	{
+		// Get the state based on the type of trajectory
+		if(this->n == 2)
+		{
+			this->quinticPosition.get_state(this->pos, this->linearVel, this->linearAcc, time);
+			this->quinticOrientation.get_state(this->quat, this->angularVel, this->angularAcc, time);
+		}
+		else
+		{
+			this->cubicPosition.get_state(this->pos, this->linearVel, this->linearAcc, time);
+			this->cubicOrientation.get_state(this->quat, this->angularVel, this->angularAcc, time);
+		}
+		
+		// Combine the information
+		//pose = Eigen::Affine3f(this->pos)*Eigen::Affine3f(this->quat);
+		
+		for(int i = 0; i < 3; i++)
+		{
+			vel[i] 	= this->linearVel[i];
+			vel[i+3]	= this->angularVel[i];
+			acc[i]		= this->linearAcc[i];
+			acc[i+3]	= this->angularAcc[i];
+		}
+	}
+}
+
+
 
 #endif
 
