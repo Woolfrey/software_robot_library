782d03a99dd64576482777b4ee840d03dfc744d7
Jon <jonathan.woolfrey@gmail.com>
2021-12-23 09:16:03 +0100

    Optimised the code, also now interpolates over angle instead of angle*axis

--- Trajectory/Quintic.h
+++ Trajectory/Quintic.h
@@ -42,7 +42,7 @@ class Quintic
 		Eigen::VectorXf p1, p2;				// Start point and end point for real numbers
 		
 		Eigen::Quaternionf q1, q2;				// Used for rotation trajectories
-		Eigen::Vector3f angleAxis;				// Interpolate over angle-axis for rotations			
+		float angle; Eigen::Vector3f axis;
 		
 		void compute_coefficients();		
 		void compute_scalars(const float &time);		// Compute the interpolation scalars for the given time	
@@ -63,7 +63,8 @@ Quintic::Quintic(const Eigen::VectorXf &startPoint,			// Constructor for traject
 {
 	if(startPoint.size() != endPoint.size())
 	{
-		// ERROR: Inputs are not the same length!
+		std::cout << "ERROR: Quintic::Quintic() : Vectors are not of equal length! The first vector has "
+			<< startPoint.size() << " elements and the second vector has " << endPoint.size() << " elements." << std::endl;
 	}
 	
 	compute_coefficients();					// Compute polynomial coefficients
@@ -78,35 +79,21 @@ void Quintic::get_state(Eigen::VectorXf &pos,				// Get the desired position for
 	// Check inputs are sound
 	if(this->isQuaternion)
 	{
-		// ERROR: Did you mean to call the one for rotations?
+		std::cout << "ERROR: Quintic::get_state() : This is an orientation trajectory, but you called a function for position!" << std::endl;
 	}
 	
 	if(pos.size() != vel.size () || vel.size() != acc.size())
 	{
-		// ERROR: Input vectors are not of the same length!
+		std::cout << "ERROR: Quintic::get_state() : Input vectors are not of equal length! The pos vector has " 
+			<< pos.size() << " elements, the vel vector has " << vel.size()
+			<< " elements and the acc vector has " << acc.size() << " elements." << std::endl;
 	}
-		
-	// Figure out where we are on the trajectory
-	if(time < this->t1)						// Not yet started
-	{
-		pos = this->p1;					// Remain at the start
-		vel.setZero();						// Don't move
-		acc.setZero();						// Don't accelerate
-	}
-	else if(time < this->t2)					// Somewhere in the middle
-	{
-		compute_scalars(time);					// Get the scalars for the current time
 	
-		pos = (1.0 - this->s)*this->p1 + this->s*this->p2;	// Position interpolation
-		vel = this->sd*(this->p2 - this->p1);			// Velocity
-		acc = this->sdd*(this->p2 - this->p1);		// Acceleration
-	}
-	else								// Finished
-	{
-		pos = this->p2;					// Remain at the end		
-		vel.setZero();						// Don't move
-		acc.setZero();						// Don't accelerate
-	}
+	compute_scalars(time);						// Get the scalars to interpolate along the trajectory
+	
+	pos = (1 - this->s)*this->p1 + this->s*this->p2;
+	vel = this->sd*(this->p2 - this->p1);
+	acc = this->sdd*(this->p2 - this->p1);
 }
 
 /******************** Create a trajectory over a rotation  ********************/
@@ -123,17 +110,13 @@ Quintic::Quintic(const Eigen::Quaternionf &startPoint,		// Constructor for orien
 {
 	compute_coefficients();					// Compute polynomial coefficients
 	
-	Eigen::Quaternionf dq = startPoint.inverse()*endPoint;	// We want to interpolate over the *difference* in rotation
+	// We want to interpolate over the difference in orientations
+	// such that: q1*dq = q2 ---> dq = conj(q1)*q2
+	Eigen::AngleAxisf angleAxis(startPoint.conjugate()*endPoint); // Do conj(q1)*q2 then convert to angle-axis
+	this->angle = angleAxis.angle();				// Get the angle encoded in the rotation
+	this->axis = angleAxis.axis();				// Get the axis of rotation
 	
-	float angle = 2*acos(dq.w());					// Angle encoded in the difference
-
-	this->angleAxis = angle*dq.vec().normalized();		// This is used later for interpolation purposes
-	
-	if(angle > M_PI)						// If the angle is greater than 180 degrees...
-	{
-		angle = 2*M_PI - angle;				// ... Take the shorter path...
-		this->angleAxis *= -1;					// ... and flip the direction to match
-	}
+	if(this->angle > M_PI) this->angle = 2*M_PI - this->angle;	// If rotation is > 180 degrees, take the shorter path
 }
 
 
@@ -146,50 +129,36 @@ void Quintic::get_state(Eigen::Quaternionf &quat,			// Get the desired rotation 
 {
 	if(!this->isQuaternion)
 	{
-		// ERROR: Did you call the right function?
+		std::cout << "ERROR: Quintic::get_state() : This is a position trajectory,"
+			<< " but you called the function for an orientation trajectory!" << std::endl;
 	}
 	
-	// Figure out where we are on the trajectory
-	if(time < this->t1)						// Not yet started
-	{
-		quat = this->q1;					// Remain at the start
-		vel.setZero();						// Don't move
-		acc.setZero();						// Don't accelerate
-	}
-	else if(time < this->t2)
-	{
-		compute_scalars(time);					// Get the scalars for the current time
-		
-		Eigen::Vector3f temp = this->s*this->angleAxis;	// Assumes zero starting rotation
-		Eigen::Quaternionf dq(Eigen::AngleAxisf(temp.norm(), temp.normalized())); // Convert to angle-axis then to quaternion
-		
-		dq = this->q1*dq;				
-		vel = this->sd*this->angleAxis;			// I hope this correct...
-		acc = this->sdd*this->angleAxis;
-	}
-	else								// Finished
-	{
-		quat = this->q2;					// Remain at the end
-		vel.setZero();						// Don't move
-		acc.setZero();						// Don't accelerate
-	}
+	compute_scalars(time);							// Get the scalar to interpolate the trajectory
 
+	float temp = this->s*this->angle;					// Compute the interpolated angle
+	Eigen::Quaternionf dq(Eigen::AngleAxisf(temp, this->axis));		// Convert to quaternion
+	
+	quat = this->q1*dq;							// q(t) = q1*dq(t)
+	vel = this->sd*this->angle*this->axis;				// Angular velocity
+	acc = this->sdd*this->angle*this->axis;				// Angular acceleration
 }
 
 
 /******************** Compute the quintic polynomial coefficients ********************/
 void Quintic::compute_coefficients()
 {
+	// NOTE: I put the error check on the time inputs here because it's common
+	// to all constructors.
 	if(this->t1 > this->t2)
 	{
-		// ERROR: Trajectory ends before it begins!
-		// Swapping times...
-		float temp = this->t1;
-		this->t1 = this->t2;
-		this->t2 = temp;
+		std::cout << "ERROR: Quintic::Quintic() : Start time of " << this->t1
+			<< " is greater than end time of " << this->t2 << "! Swapping the times..." << std::endl;
+		float temp = this->t2;
+		this->t2 = this->t1;
+		this->t1 = temp;
 	}
 	
-	float dt = this->t1 - this->t2;				// Time difference
+	float dt = this->t1 - this->t2;						// Time difference
 	
 	this->a =   6*pow(dt,-5);
 	this->b = -15*pow(dt,-4);
@@ -199,7 +168,7 @@ void Quintic::compute_coefficients()
 /******************** Compute the scalars for the time interpolation ********************/
 void Quintic::compute_scalars(const float &time)
 {
-	float dt = time - this->t1;					// Get elapsed time since the beginning
+	float dt = time - this->t1;							// Get elapsed time since the beginning
 	
 	this->s =      this->a*pow(dt,5) +    this->b*pow(dt,4) +   this->c*pow(dt,3);
 	this->sd =   5*this->a*pow(dt,4) +  4*this->b*pow(dt,3) + 3*this->c*pow(dt,2);
