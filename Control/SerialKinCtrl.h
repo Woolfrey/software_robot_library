48181ff9768b8b22657a9cf67c0d984607be8be0
Jon <jonathan.woolfrey@gmail.com>
2021-12-26 15:53:45 +0100

    Working on joint control

--- Control/SerialKinCtrl.h
+++ Control/SerialKinCtrl.h
@@ -1,26 +1,104 @@
 #include <CartesianTrajectory.h>
 #include <MultiPointTrajectory.h>
+#include <SerialLink.h>
 
 class SerialKinCtrl
 {
 	public:
 		SerialKinCtrl() {}
 		
-		// Get Functions
-		Eigen::VectorXf get_pose_error(const Eigen::Isometry3f &desired,
-						const Eigen::Isometry3f &actual);
+		SerialKinCtrl(SerialLink *serial);
 		
-		// Control Functions
-		void move_to_position(const Eigen::VectorXf &position);	// Move joints to a desired position
-		void move_to_pose(const Eigen::Isometry3f &pose);		// Move endpoint to a desired position, orientation
+		// Set Functions
+		bool set_joint_target(const Eigen::VectorXf &target);
+		bool set_joint_targets(const std::vector<Eigen::VectorXf> &targets, const std::vector<float> &times);
+		bool set_proportional_gain(const float &p);
+		
+		// Get Functions
+		Eigen::VectorXf get_joint_control(const float &time);		// Control to track joint trajectory
+		Eigen::VectorXf get_pose_error(const Eigen::Isometry3f &desired, const Eigen::Isometry3f &actual);
 	
-	protected:								// SerialDynControl has access to these
+	private:									// SerialDynControl has access to these
 	
-	private:
-		CartesianTrajectory cartesianTrajectory;
+		float kp = 1.0;							// Proportional gain
+
+		SerialLink *robot;							// Use pointer to memory address
+
 		MultiPointTrajectory jointTrajectory;
+		
+		Eigen::VectorXf q_d, qdot_d, qddot_d;					// Desired joint position, velocity
 	
-};										// Semicolon needed after class declaration
+};											// Semicolon needed after class declaration
+
+/******************** Set a desired joint configuration to move to ********************/
+bool SerialKinCtrl::set_joint_target(const Eigen::VectorXf &target)
+{
+	int n = this->robot->get_number_of_joints();					// Number of joints
+	if(target.size() != n)
+	{
+		std::cout << "ERROR: SerialKinCtrl::set_joint_target() : Input vector has "
+			<< target.size() << " elements, but the robot has " << n << " joints!"
+			<< " Joint target has not been set." << std::endl;
+			
+		return false;
+	}
+	else
+	{
+		float dt;
+		float dq;
+		float endTime = 1.0;
+		
+		for(int i = 0; i < n; i++)
+		{
+			// Check that the target is within joint limits
+			// if(target[i] >= this->robot.link[i].qlim[1]) 	target[i] = 0.99*this->robot.link[i].pLim[1];
+			// else if(target <= this->robot.link[i].qlim[0])	target[i] = 0.99*this->robot.link[i].pLim[0];	
+			
+			// Compute the optimal time scaling for quintic polynomial
+			// Angeles, J. (2002). Fundamentals of robotic mechanical systems (Vol. 2).
+			// New York: Springer-Verlag.
+			
+			// dq = this->target[i] - this->robot.q[i];				// Distance travelled
+			// if(dq > 0)		dt = (15*dq)/(8*this->robot.link[i].vLim[1]);	// Time with peak velocity at max. speed
+			// else if(dq < 0)	dt = (15*dq)/(8*this->robot.link[i].vLim[0]);	// Time with peak velocity at min. speed
+			// if(dq != 0 && dt > endTime) endTime = dt;				// Set new end time
+		}
+		
+		// this->jointTrajectory  = MultiPointTrajectory(q, target, 0, endTime);	// Set a new trajectory object
+		return true;
+	}
+}
+
+bool SerialKinCtrl::set_joint_targets(const std::vector<Eigen::VectorXf> &targets, const std::vector<float> &times)
+{
+	std::cout << "Worker bees can leave." << std::endl;
+	std::cout << "Even drones can fly away." << std::endl;
+	std::cout << "The Queen is their slave." << std::endl;
+	
+	return true;
+}
+
+/******************** Set proportional gains for the feedback control ********************/
+bool SerialKinCtrl::set_proportional_gain(const float &p)
+{
+	if(p < 0)
+	{
+		std::cout << "ERROR: SerialKinCtrl::set_proportional_gain : Proportional gain cannot be negative!" << std::endl;
+		return false;
+	}
+	else
+	{
+		this->kp = p;
+		return true;
+	}
+}
+
+/******************** Compute the control to track the internal joint trajectory ********************/
+Eigen::VectorXf SerialKinCtrl::get_joint_control(const float &time)
+{
+	this->jointTrajectory.get_state(this->q_d, this->qdot_d, this->qddot_d, time); 	// Get the desired state for the given time
+	return this->qdot_d + this->kp*(this->q_d - this->robot->get_joint_position());	// Feedforward + feedback
+}
 
 /******************** Get the error between two poses for feedback control purposes ********************/
 Eigen::VectorXf SerialKinCtrl::get_pose_error(const Eigen::Isometry3f &desired,
