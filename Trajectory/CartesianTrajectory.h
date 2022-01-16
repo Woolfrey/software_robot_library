9504b6acf0668cba3a0318eb761a19bac5cdcaed
Jon <jonathan.woolfrey@gmail.com>
2022-01-16 11:48:45 +0100

    Improved error checking, messages

--- Trajectory/CartesianTrajectory.h
+++ Trajectory/CartesianTrajectory.h
@@ -5,152 +5,147 @@
 #ifndef CARTESIAN_TRAJECTORY_H_
 #define CARTESIAN_TRAJECTORY_H_
 
-#include <CubicSpline.h>
-#include <Quintic.h>
+#include <CubicRot.h>
+#include <QuinticRot.h>
 
 class CartesianTrajectory
 {
 	public:
-		// Constructor(s)
 		CartesianTrajectory() {}
 		
 		CartesianTrajectory(const Eigen::Isometry3f &startPose,
-				const Eigen::Isometry3f &endPose,
-				const float &startTime,
-				const float &endTime);
+					const Eigen::Isometry3f &endPose,
+					const float &startTime,
+					const float &endTime);
 		
-		CartesianTrajectory(const std::vector<Eigen::Isometry3f> &poses,
-				const std::vector<float> &times);
+		CartesianTrajectory(const std::vector<Eigen::Isometry3f> &poses, const std::vector<float> &times);
 		
-		// Get Functions
-		void get_state(Eigen::Isometry3f &pose,			// Get the desired state for the given time
-				Eigen::VectorXf &vel,
-				Eigen::VectorXf &acc,
-				const float &time);
+		bool get_state(Eigen::Isometry3f &pose, Eigen::VectorXf &vel, Eigen::VectorXf &acc, const float &time);
 		
 	private:
-		int n;								// Number of waypoints
-		
-		// Trajectories for n > 2
-		CubicSpline cubicPosition;
-		CubicSpline cubicOrientation;
-		
-		// Trajectories for n = 2
-		Quintic quinticPosition;
-		Quintic quinticOrientation;
-		
-		// These variables are used to combine position & orientation
-		Eigen::VectorXf pos, linearVel, linearAcc;			// Trajectory classes only accept VectorXf for translation
-		Eigen::Vector3f angularVel, angularAcc;
-		Eigen::Quaternionf quat;
-		
+		bool isNotValid = true;
+		CubicSpline translationTrajectory;
+		CubicRot rotationTrajectory;
+		Eigen::Isometry3f T0;
+				
 };										// Semicolon needed after a class declaration
 
-/******************** Constructor ********************/
+/******************** Constructor with 2 points ********************/
 CartesianTrajectory::CartesianTrajectory(const Eigen::Isometry3f &startPose,
 					const Eigen::Isometry3f &endPose,
 					const float &startTime,
 					const float &endTime)
-					:
-					n(2),
-					pos(Eigen::VectorXf(3)),		// The base trajectory classes need VectorXf for translation
-					linearVel(Eigen::VectorXf(3)),
-					linearAcc(Eigen::VectorXf(3))
+					: T0(startPose)
 {
-	this->quinticPosition = Quintic(startPose.translation(),
-					endPose.translation(),
-					startTime,
-					endTime);
-
-	this->quinticOrientation = Quintic(Eigen::Quaternionf(startPose.rotation()),
-					Eigen::Quaternionf(endPose.rotation()),
-					startTime,
-					endTime);
+	//Check the times are in correct order
+	double t1 = startTime;
+	double t2 = endTime;
+	if(t1 > t2)
+	{
+		std::cerr 	<< "[WARNING][CARTESIANTRAJECTORY] Constructor : Start time of "
+				<< t1 << " is greater than end time of " << t2 << ". Swapping values..." << std::endl;
+		double temp = t1;
+		t1 = t2;
+		t2 = temp;
+	}
+	
+	std::vector<float> times;
+	times.push_back(t1);
+	times.push_back(t2);
+	
+	std::vector<Eigen::VectorXf> pos;
+	pos.push_back(startPose.translation());
+	pos.push_back(endPose.translation());
+	this->translationTrajectory = CubicSpline(pos, times);
+	
+	std::vector<Eigen::Quaternionf> rot;
+	rot.push_back(Eigen::Quaternionf(startPose.rotation()));
+	rot.push_back(Eigen::Quaternionf(endPose.rotation()));
+	this->rotationTrajectory = CubicRot(rot, times);
 }
 
-/******************** Constructor ********************/
+/******************** Constructor with multiple points ********************/
 CartesianTrajectory::CartesianTrajectory(const std::vector<Eigen::Isometry3f> &poses,
 					const std::vector<float> &times)
-					:
-					n(poses.size()),
-					pos(Eigen::VectorXf(3)),
-					linearVel(Eigen::VectorXf(3)),
-					linearAcc(Eigen::VectorXf(3))
+					: T0(poses[0])
 {
-	// Check the inputs are sound
+	// Check the input arguments are of the same length
 	if(poses.size() != times.size())
 	{
-		std::cout << "ERROR: CartesianTrajectory::CartesianTrajectory() : Input vectors are not of equal length!" << std::endl;
-		std::cout << "Pose vector has " << poses.size() << " elements and the time vector has " << times.size() << " elements.";
-	}
-	
-	// Create minimum jerk trajectory
-	if(this->n == 2)
-	{
-		this->quinticPosition = Quintic(poses[0].translation(),
-						poses[1].translation(),
-						times[0],
-						times[1]);
-						
-		this->quinticOrientation = Quintic(Eigen::Quaternionf(poses[0].linear()),
-						    Eigen::Quaternionf(poses[1].linear()),
-						    times[0],
-						    times[1]);
-	}
-	
-	// Create minimum acceleration trajectory
-	else if(this->n > 2)
-	{
-		std::vector<Eigen::VectorXf> position;
-		std::vector<Eigen::Quaternionf> orientation;
-		
-		for(int i = 0; i < this->n; i++)
-		{
-			position.push_back(poses[i].translation());
-			orientation.push_back(Eigen::Quaternionf(poses[i].linear()));
-		}
-		
-		this->cubicPosition = CubicSpline(position, times);
-		this->cubicOrientation = CubicSpline(orientation, times);
-	}
-	else std::cout << "ERROR: CartesianTrajectory::CartesianTrajectory() : At least 2 poses are needed to generate a trajectory!" << std::endl;
-}
-
-/******************** Get the desired state for the given time ********************/
-void CartesianTrajectory::get_state(Eigen::Isometry3f &pose,
-				Eigen::VectorXf &vel,
-				Eigen::VectorXf &acc,
-				const float &time)
-{
-	if(vel.size() != 6 || acc.size() != 6)
-	{
-		std::cout << "ERROR: CartesianTrajectory::get_state() : Velocity and acceleration vectors must have 6 elements!" << std::endl;
-		std::cout << "You input vel with " << vel.size() << " elements and acc with " << acc.size() << " elements.";
+		std::cerr << "[ERROR][CARTESIANTRAJECTORY] Constructor : Input arguments are not of equal length." << std::endl;
+		std::cerr << " poses: " << poses.size() << " times: " << times.size() << std::endl;
 	}
 	else
 	{
-		// Get the state based on the type of trajectory
-		if(this->n == 2)
+		this->isNotValid = false;
+		
+		// Check that the times are in ascending order
+		for(int i = 0; i < times.size() - 1; i++)
 		{
-			this->quinticPosition.get_state(this->pos, this->linearVel, this->linearAcc, time);
-			this->quinticOrientation.get_state(this->quat, this->angularVel, this->angularAcc, time);
-		}
-		else
-		{
-			this->cubicPosition.get_state(this->pos, this->linearVel, this->linearAcc, time);
-			this->cubicOrientation.get_state(this->quat, this->angularVel, this->angularAcc, time);
+			if(times[i] > times[i+1])
+			{
+				std::cerr << "[ERROR][CARTESIANTRAJECTORY] Constructor : Times are not in ascending order." << std::endl;
+				this->isNotValid = true;
+				break;
+			}
 		}
 		
-		// Combine the information
-		pose = Eigen::Translation3f(this->pos)*this->quat;		// Transform should be translation x rotation
+		if(!this->isNotValid) // this is valid
+		{		
+			std::vector<Eigen::VectorXf> pos;
+			std::vector<Eigen::Quaternionf> rot;
+			for(int i = 0; i < poses.size(); i++)
+			{
+				pos.push_back(poses[i].translation());
+				rot.push_back(Eigen::Quaternionf(poses[i].rotation()));
+			}
+			
+			this->translationTrajectory = CubicSpline(pos, times);
+			this->rotationTrajectory = CubicRot(rot, times);
+		}
+	}
+}
+
+/******************** Get the desired state for the given time ********************/
+bool CartesianTrajectory::get_state(Eigen::Isometry3f &pose, Eigen::VectorXf &vel, Eigen::VectorXf &acc, const float &time)
+{
+	// Variables used in this scope
+	Eigen::VectorXf pos(3), linearVel(3), linearAcc(3);
+	Eigen::Vector3f angularVel(3), angularAcc(3);
+	Eigen::Quaternionf rot;
+	
+	if(vel.size() != 6 || acc.size() != 6)
+	{
+		std::cerr << "[ERROR][CARTESIANTRAJECTORY] get_state() : Velocity and acceleration vectors must have 6 elements." << std::endl;
+		std::cerr << " vel: " << vel.size() << " acc: " << acc.size() << std::endl;
+			
+		pose = this->T0;						// Remain at the start
+		vel.setZero(); acc.setZero();					// Don't move
+		
+		return false;
+	}
+	else if(this->translationTrajectory.get_state(pos, linearVel, linearAcc, time)
+	     && this->rotationTrajectory.get_state(rot, angularVel, angularAcc, time))
+	{
+		pose = Eigen::Translation3f(pos)*rot;				// Transform should be translation x rotation
 		
 		for(int i = 0; i < 3; i++)
 		{
-			vel[i] 	= this->linearVel[i];
-			acc[i]		= this->linearAcc[i];
-			vel[i+3]	= this->angularVel[i];
-			acc[i+3]	= this->angularAcc[i];
+			vel[i]   = linearVel[i];
+			vel[i+3] = angularVel[i];
+			acc[i]   = linearAcc[i];
+			acc[i+3] = angularAcc[i];
 		}
+		return true;
+	}
+	else
+	{
+		std::cerr << "[ERROR][CARTESIANTRAJECTORY] get_state() : Could not obtain the state." << std::endl;
+		
+		pose = this->T0;						// Remain at the start
+		vel.setZero(); acc.setZero();					// Don't move
+		
+		return false;
 	}
 }
 
