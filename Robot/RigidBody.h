33112fa886526c5526a9e3f17fcac3a33a7d4666
Jon <jonathan.woolfrey@gmail.com>
2022-01-30 18:06:27 +0100

    Seems to be working correctly

--- Robot/RigidBody.h
+++ Robot/RigidBody.h
@@ -1,3 +1,5 @@
+#ifndef RIGIDBODY_H_
+#define RIGIDBODY_H_
 
 #include <Eigen/Geometry>								// Eigen::Affine3f
 
@@ -6,15 +8,13 @@ class RigidBody
 	public:
 		RigidBody() {}								// Empty constructory
 		
-		RigidBody(const Eigen::Isometry3f &_pose) : pose(_pose) {}		// Constructor with just kinematics
-		
-		RigidBody(const Eigen::Isometry3f &_pose,				// Constructor with kinematics & dynamics
-			const float &_mass,
-			const Eigen::Vector3f &_com,
-			const Eigen::Matrix3f &_inertia);
+		RigidBody(const Eigen::Isometry3f &origin,				// Origin relative to some other frame
+			const Eigen::Vector3f &centreOfMass,				// Center of mass rleative to origin
+			const float &_mass,						// Mass (kg)
+			const Eigen::VectorXf &momentOfInertia);			// Inertia (Nm^2)
 			
 		// Set Functions
-		bool set_state(const Eigen::Isometry3f &_pose,
+		bool set_state(const Eigen::Isometry3f &origin,
 				const Eigen::Vector3f &linearVel,
 				const Eigen::Vector3f &angularVel);
 		
@@ -25,27 +25,46 @@ class RigidBody
 		float get_mass() const {return this->mass;}
 		
 	private:
+		// Kinematic Properties
 		Eigen::Isometry3f pose;						// Pose of the origin to the local frame
+		Eigen::Vector3f linearVel = {0.0 , 0.0 , 0.0};
+		Eigen::Vector3f angularVel = {0.0 , 0.0 , 0.0};
+				
+		// Dynamic Properties
 		Eigen::Matrix3f inertia;						// Moment of inertia at the center of mass
 		Eigen::Matrix3f Idot;							// Time-derivative of inertia
 		Eigen::Vector3f com = {0.5, 0.0, 0.0};				// Location of center of mass relative to local frame
-		Eigen::Vector3f linearVel = {0.0 , 0.0 , 0.0};
-		Eigen::Vector3f angularVel = {0.0 , 0.0 , 0.0};
 		float mass = 1.0;							// Mass of the rigid body
 		
 };											// Semicolon needed after a class declaration
 
 /******************** Full constructor with dynamic properties ********************/
-RigidBody::RigidBody(const Eigen::Isometry3f &_pose,
+RigidBody::RigidBody(const Eigen::Isometry3f &origin,
+			const Eigen::Vector3f &centreOfMass,
 			const float &_mass,
-			const Eigen::Vector3f &_com,
-			const Eigen::Matrix3f &_inertia)
-			: pose(_pose)
+			const Eigen::VectorXf &momentOfInertia)
+			: pose(origin)
+			, com(centreOfMass)
 			, mass(_mass)
-			, com(_com)
-			, inertia(_inertia)
 {
-	// Worker bees can leave.
-	// Even drones can fly away.
-	// The Queen is their slave.
+	if(momentOfInertia.size() != 6)
+	{
+		std::cerr << "[ERROR] [RIGIDBODY] Constructor : Expected 6 elements for the momentOfInertia argument. "
+			<< " Your input had " << momentOfInertia.size() << " elements." << std::endl;
+	}
+	else
+	{
+		this->inertia << momentOfInertia(0), momentOfInertia(1), momentOfInertia(2),
+				  momentOfInertia(1), momentOfInertia(3), momentOfInertia(4),
+				  momentOfInertia(2), momentOfInertia(4), momentOfInertia(5);
+	}
+				
+	if(this->mass < 0)
+	{
+		std::cerr << "[ERROR] [RIGIDBODY] Constructor : Mass cannot negative. Your input was " << this->mass << "."
+			<< " Ensuring the value is positive to avoid problems..." << std::endl;
+		this->mass *= -1;
+	}
 }
+
+#endif
