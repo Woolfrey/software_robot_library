89b152f2849a9ddaa6648be539a43102f6580e18
Jon <jonathan.woolfrey@gmail.com>
2022-08-03 18:20:25 +0200

    New output for LU decomposition, moved QP to its own file

--- Test/math_test.cpp
+++ Test/math_test.cpp
@@ -1,5 +1,5 @@
 #include <iostream>                                                                                 // std::cout 
-#include <QPSolver.h>
+#include <Math.h>
 #include <time.h>                                                                                   // clock, clock_t, CLOCKS_PER_SEC
 
 int main(int argc, char *argv[])
@@ -8,16 +8,92 @@ int main(int argc, char *argv[])
 	
 	// Variables used in this scope
 	clock_t timer;
+	float time, hertz;
 	int m, n;
-	Eigen::MatrixXf A;
+	Eigen::MatrixXf A, invA;
 	Eigen::VectorXf y;
-	Eigen::VectorXf x;
-	Eigen::VectorXf x0;
-	Eigen::VectorXf xHat;
+	Eigen::VectorXf x, xHat;
+	Eigen::MatrixXf comparison;
 	
 	
 	std::cout << "\n************************************************************" << std::endl;
-	std::cout <<   "*                   CHOLESKY DECOMPOSITION                 *" << std::endl;
+	std::cout <<   "*                      LU DECOMPOSITION                    *" << std::endl;
+	std::cout <<   "************************************************************" << std::endl;
+	
+	m = 6;
+	n = 6;
+	A = Eigen::MatrixXf::Random(m,n);
+	A.col(n-2) = A.col(0); // Force a singularity
+	
+	std::cout << "\nWe can express a matrix A = LU, where L is a lower-triangular "
+	          << "and U is upper-triangular." << std::endl;
+	          
+	std::cout << "\nHere is the matrix A:\n" << std::endl;
+	std::cout << A << std::endl;
+	
+	Eigen::MatrixXf L, U;
+	timer = clock();
+	get_lu_decomposition(A,L,U);
+	timer = clock() - timer;
+	time  = (float)timer/CLOCKS_PER_SEC;
+	
+	std::cout << "\nHere is the matrix L:\n" << std::endl;
+	std::cout << L << std::endl;
+	
+	std::cout << "\nAnd here is the matrix U:\n" << std::endl;
+	std::cout << U << std::endl;
+	
+	std::cout << "\nThe error norm ||A-L*U|| is " << (A-L*U).norm() << ". "
+	          << "It took " << time*1000 << " ms to solve (" << 1/time << " Hz)." << std::endl;
+	
+	std::cout << "\nWe can use the triangular property to get a fast inverse.\n" << std::endl;
+	timer = clock();
+	invA = get_inverse(A);
+	timer = clock() - timer;
+	time = (float)timer/CLOCKS_PER_SEC;
+	
+	std::cout << "\nHere is the inverse of A:\n" << std::endl;
+	std::cout << invA << std::endl;
+	
+	std::cout << "\nIt took " << time*1000 << " ms to compute (" << 1/time << " Hz)." << std::endl;
+	
+	std::cout << "\nHere is A times its inverse:\n" << std::endl;
+	std::cout << A*invA << std::endl;
+	
+	std::cout << "\nWe can use the triangular property to solve a system of equations y = A*x "
+	          << "without having to compute the inverse matrix directly.\n" << std::endl;
+	x = Eigen::VectorXf::Random(n);
+	y = A*x;
+	
+	std::cout << "\nHere is the vector x:\n" << std::endl;
+	std::cout << x << std::endl;
+	
+	std::cout << "\nAnd here is y = A*x:\n" << std::endl;
+	std::cout << y << std::endl;
+	
+	timer = clock();
+	xHat  = solve_linear_system(y,A,Eigen::VectorXf::Random(n));
+	timer = clock() - timer;
+	time  = (float)timer/CLOCKS_PER_SEC;	
+	
+	std::cout << "\nThen we call solve_linear_system(y,A) to get the estimate x'. "
+		  << "Here is x and x' side by side:\n" << std::endl;
+
+	comparison.resize(n,2);
+	comparison.col(0) = x;
+	comparison.col(1) = xHat;
+	
+	std::cout << comparison << std::endl;
+	
+	std::cout << "\nThe error norm ||y-A*x'|| is " << (y-A*xHat).norm() << ". "
+	          << "It took " << time*1000 << " ms to solve (" << 1/time << " Hz)." << std::endl;
+	          
+	std::cout << "\nNotice that get_inverse(A) is not robust to singularities "
+	          << "because it cannot return a proper inverse since A*invA =/= I. "
+	          << "However, solve_linear_system(y,A) is robust!" << std::endl;
+	
+	std::cout << "\n************************************************************"   << std::endl;
+	std::cout <<   "*                   CHOLESKY DECOMPOSITION                 *"   << std::endl;
 	std::cout <<   "************************************************************\n" << std::endl;
 	
 	m = 5;
@@ -25,47 +101,60 @@ int main(int argc, char *argv[])
 	A = Eigen::MatrixXf::Random(m,n);
 	A = A*A.transpose();
 	
-	std::cout << "\nWe can use Cholesky decomposition to express a positive-definite matrix A as L*L'.\n" << std::endl;
-	std::cout << "\nHere is the matrix A:\n" << std::endl;
+	/**************************** General test of Cholesky decomposition **********************/
+	std::cout << "\nWe can use Cholesky decomposition to express a positive-definite matrix A as L*L'. "
+	          << "\nHere is the matrix A:\n" << std::endl;
+	          
 	std::cout << A << std::endl;
 	
 	timer = clock();
-	Eigen::MatrixXf L = get_cholesky_decomp(A);
+	L = get_cholesky_decomposition(A);
 	timer = clock() - timer;
+	time  = (float)timer/CLOCKS_PER_SEC;
 	
 	std::cout << "\nHere is the matrix L:\n" << std::endl;
 	std::cout << L << std::endl;
-	std::cout << "\nIt took " << (float)timer/CLOCKS_PER_SEC*1000 << "ms to solve ("
-	          <<  1/((float)timer/CLOCKS_PER_SEC) << " Hz).\n" << std::endl;
-	          
+	std::cout << "\nIt took " << time*1000 << "ms to solve (" << 1/time << " Hz)." << std::endl;
 	std::cout << "\nThe error norm ||A - L*L'|| is: " << (A - L*L.transpose()).norm() << ".\n" << std::endl;
 	          
+	/****************** Solving linear systems with a positive-definite matrix ****************/
 	std::cout << "\nIf A is part of a linear system y = A*x, give y we can solve for x.\n" << std::endl;
-	
+	std::cout << "\nHere is the vector x:\n" << std::endl;
 	x = Eigen::VectorXf::Random(n);
+	std::cout << x << std::endl;
+	
+	std::cout << "\nAnd here is y = A*x:\n" << std::endl;
 	y = A*x;
-	timer = clock();
-	xHat = solve_cholesky_system(y,A);
-	timer = clock() - timer;
+	std::cout << y << std::endl;
 	
-	std::cout << "\nThe error for the estimate of x is: " << (x - xHat).norm() << ". "
-	          << "It took " << (float)timer/CLOCKS_PER_SEC*1000 << "ms to solve ("
-	          << 1/((float)timer/CLOCKS_PER_SEC) << " Hz).\n" << std::endl;
+	timer = clock();
+	xHat  = solve_cholesky_system(y,A);
+	timer = clock() - timer;
+	time  = (float)timer/CLOCKS_PER_SEC;
+	
+	std::cout << "\nHere is x and the estimate of x side by side:\n" << std::endl;
+	comparison.resize(n,2);
+	comparison.col(0) = x;
+	comparison.col(1) = xHat;
+	std::cout << comparison << std::endl;
+	
+	std::cout << "\nThe error norm ||x - x'|| is " << (x-xHat).norm() << ", "
+	          << "and it took " << time*1000 << " ms to solve (" << 1/time << " Hz)." << std::endl;
+	
 	          
-	
 	std::cout << "\nWe can also use the triangular property to compute the inverse of A.\n" << std::endl;
-	std::cout << "\nHere is the inverse of A:\n" << std::endl;
 	timer = clock();
-	Eigen::MatrixXf invA = get_cholesky_inverse(A);
+	invA = get_cholesky_inverse(A);
 	timer = clock() - timer;
 	
+	std::cout << "\nHere is the inverse of A:\n" << std::endl;
 	std::cout << invA << std::endl;
 	
-	std::cout << "\nIt took " << (float)timer/CLOCKS_PER_SEC*1000 << " ms to solve ("
-	          << 1/((float)timer/CLOCKS_PER_SEC) << " Hz)." << std::endl;
-	          
+	std::cout << "\nIt took " << time*1000 << " ms to solve (" << 1/time << " Hz)." << std::endl;
+	
 	std::cout << "\nHere is A*inv(A):\n" << std::endl;
 	std::cout << A*invA << std::endl;
+	
 	
 	std::cout << "\n************************************************************" << std::endl;
 	std::cout <<   "*                      QR DECOMPOSITION                    *" << std::endl;
@@ -73,13 +162,15 @@ int main(int argc, char *argv[])
 	
 	m = 6;
 	n = 5;
-	
 	A = Eigen::MatrixXf::Random(m,n);
+	A.col(0) = A.col(n-2);                                                                      // Force a singularity
 	
-	A.col(0) = A.col(n-2);                                                                        // Force a singularity
-	std::cout << "\nHere is a random matrix:\n" << std::endl;
+	/*************************** Demonstration of QR decomposition ***************************/
+	std::cout << "\nHere is a random " << m << "x" << n << " matrix:\n" << std::endl;
 	std::cout << A << std::endl;
-
+	
+	std::cout << "\nIt can be decomposed in to A = Q*R, where Q is orthogonal and R "
+	          << "is upper triangular.\n" << std::endl;
 	
 	Eigen::MatrixXf Q, R;
 	if( get_qr_decomposition(A,Q,R) )
@@ -92,138 +183,28 @@ int main(int argc, char *argv[])
 		std::cout << R << std::endl;
 	}
 	
-	std::cout << "\nHere is Q*R:\n" << std::endl;
-	std::cout << Q*R << std::endl;
+	std::cout << "\nNotice that A is singular and the corresponding diagonal element of "
+	          << "R is close to zero: " << R(n-2,n-2) << ".\n" << std::endl;
 	
-	std::cout << "\nThe error norm || A - Q*R || is: " << (A-Q*R).norm() << "." << std::endl;
+	std::cout << "\nThe QR decomposition is robust to singularities. "
+                  << "The error norm ||A - Q*R|| is: " << (A-Q*R).norm() << "." << std::endl;
+
+	std::cout << "\nThe null space matrix N can be computed with I - Q*Q':\n" << std::endl;
+	int dim = std::max(m,n);
+	Eigen::MatrixXf N = Eigen::MatrixXf::Identity(dim,dim) - Q*Q.transpose();
+	std::cout << N << std::endl;
 	
-	if( m > n )
+	if(m > n)
 	{
-		std::cout << "\nThe null space of A is:\n" << std::endl;
-		std::cout << Eigen::MatrixXf::Identity(m,m) - Q*Q.transpose() << std::endl;
+		std::cout << "\nHere is N*A:\n" << std::endl;
+		std::cout << N*A << std::endl;
+	}
+	else if(m < n)
+	{
+		std::cout << "\nHere is A*N:\n" << std::endl;
+		std::cout << A*N << std::endl;
 	}
 
-
-	std::cout << "\n************************************************************" << std::endl;
-	std::cout <<   "*             SOLVING LINEAR SYSTEMS WITH QR               *" << std::endl;
-	std::cout <<   "************************************************************\n" << std::endl;
-	
-	std::cout << "\nHere is a random system of equations y = A*x.\n" << std::endl;
-	
-	m = 6;
-	n = 6;
-	
-	A = Eigen::MatrixXf::Random(m,n);
-	x = Eigen::VectorXf::Random(n);
-	y = A*x;
-	
-	std::cout << "\ny:\n" << std::endl;
-	std::cout << y << std::endl;
-	std::cout << "\nA:\n" << std::endl;
-	std::cout << A << std::endl;
-
-	std::cout << "\nUsing QR decomposition, the estimate for x is:\n" << std::endl;
-	
-	timer   = clock();
-	xHat = solve_linear_system(y,A,x);
-	timer    = clock() - timer;
-	std::cout << xHat << std::endl;
-	
-	std::cout << "\nAnd here was the original vector:\n" << std::endl;
-	std::cout << x << std::endl;
-	
-	std::cout << "\nThe solution error is " << (x - xHat).norm() << std::endl;
-	
-	std::cout << "\nIt took " << (float)timer/CLOCKS_PER_SEC*1000 << "ms to solve ("
-	          << 1/((float)timer/CLOCKS_PER_SEC) << "Hz).\n" << std::endl;
-
-	std::cout << "\n************************************************************" << std::endl;
-	std::cout <<   "*               QUADRATIC PROGRAMMING (QP)                 *" << std::endl;
-	std::cout <<   "************************************************************\n" << std::endl;
-	
-	
-	// Test an underdetermined system
-	m = 6;
-	n = 4;
-	A = Eigen::MatrixXf::Random(m,n);
-	x = Eigen::VectorXf::Random(n);
-	y = A*x;
-	
-	std::cout << "\nHere is the matrix for a random, underdetermined system y = A*x:\n" << std::endl;
-	std::cout << A << std::endl;
-	
-	std::cout << "\nAnd here is the output values y:\n" << std::endl;
-	std::cout << y << std::endl;
-	
-	std::cout << "\nWe can use linear least squares to get an estimate for the input vector x." << std::endl;
-	timer = clock();
-	xHat = QPSolver::least_squares(y,A,Eigen::MatrixXf::Identity(m,m), Eigen::VectorXf::Zero(n));
-	timer = clock() - timer;
-	
-	std::cout << "The estimate is:\n" << std::endl;
-	std::cout << xHat << std::endl;
-	
-	std::cout << "\nIt took " << (float)timer/CLOCKS_PER_SEC*1000<< "ms to solve ("
-	          << 1/((float)timer/CLOCKS_PER_SEC) << "Hz).\n" << std::endl;
-	std::cout << "\nThe solution error was " << (x-xHat).norm() << std::endl;
-	
-	m = 6;
-	n = 7;
-	A = Eigen::MatrixXf::Random(m,n);
-	x = Eigen::VectorXf::Random(n);
-	y = A*x;
-
-	std::cout << "\nNow here is a random, over-determined system with matrix A:\n" << std::endl;
-	std::cout << A << std::endl;
-	std::cout << "\nAnd the vector y:\n" << std::endl;
-	std::cout << y << std::endl;
-	
-	timer = clock();
-	xHat = QPSolver::least_squares(Eigen::VectorXf::Zero(n),                                    // Desired value for xd
-	                               Eigen::MatrixXf::Identity(n,n),                              // Weighting matrix W on solution
-	                               y,
-	                               A,
-	                               Eigen::VectorXf::Zero(n));
-	timer = clock() - timer;
-	
-	std::cout << "\nThe solution error ||y-A*x|| is: " << (y-A*xHat).norm() << ".\n" << std::endl;
-	std::cout << "\nIt took " << (float)timer/CLOCKS_PER_SEC*1000 << "ms to solve ("
-	          << 1/((float)timer/CLOCKS_PER_SEC) << "Hz).\n" << std::endl;
-	          
-	          
-	std::cout << "\nWe can put constraints on the solution. "
-	          << "We need to create a QPSolver object for this one since it relies on the Interior Point method." << std::endl;
-	QPSolver solver;
-	m = 6;
-	n = 6;
-	x.resize(n);
-	x << 2, 1, -3, 3 ,2, -2;
-	A = Eigen::MatrixXf::Random(m,n);
-	y = A*x;
-	Eigen::VectorXf xMax(n);
-	Eigen::VectorXf xMin(n);
-	xMax = 4*Eigen::VectorXf::Ones(n);
-	xMin = -4*Eigen::VectorXf::Ones(n);
-	
-	timer = clock();
-	x0.resize(n);
-	x0 << 2, 1, -3, 3, 2, -2;
-	xHat = solver.least_squares(y,A,Eigen::MatrixXf::Identity(m,m),xMin,xMax,x0);
-	timer = clock() - timer;
-	
-	std::cout << "\nHere is xMax x xMin:\n" << std::endl;
-	Eigen::MatrixXf result(3,n);
-	result.row(0) = xMax.transpose();
-	result.row(1) = xHat.transpose();
-	result.row(2) = xMin.transpose();
-	
-	std::cout << result << std::endl;
-	
-	std::cout << "\nIt took " << (float)timer/CLOCKS_PER_SEC*1000 << "ms to solve. ("
-	          << 1/((float)timer/CLOCKS_PER_SEC) << "Hz)." << std::endl;
-	          
-	std::cout << "\nThe solution error is " << ((y - A*xHat).norm())/y.norm()*100<< "% .\n" << std::endl;
-	
 	return 0;                                                                                   // No problems with main()
 }
 
