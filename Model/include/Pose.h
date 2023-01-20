393ef3d074944d8423394e15de403837ca41fd86
Jon <jonathan.woolfrey@gmail.com>
2023-01-20 12:32:06 +1100

    Compiled but needs testing

--- Model/include/Pose.h
+++ Model/include/Pose.h
@@ -7,26 +7,35 @@
 #ifndef POSE_H_
 #define POSE_H_
 
-#include <Eigen/Geometry>
+#include <Eigen/Geometry>                                                                           // Eigen::Quaternionf
+#include <iostream>                                                                                 // std::cout
 
 class Pose
 {
 	public:
-
+		Pose() {}                                                                           // Empty constructor
+		
 		Pose(const Eigen::Vector3f &position, const Eigen::Quaternionf &quaternion);        // Constructor
 		
 		// Functions
-		Eigen::Matrix<float,6,1> get_pose_error(const Pose &desired);                       // Error from a desired pose
+	
+		Eigen::Matrix<float,6,1> error(const Pose &desired);                                // Error from a desired pose
 		
-		Eigen::Matrix<float,4,4> get_matrix();                                              // Return a 4x4 Homogeneous Transform
+		Eigen::Matrix<float,4,4> as_matrix();                                               // Return a 4x4 Homogeneous Transform
+
+		Eigen::Vector3f pos() const {return this->_pos;}
+		
+		Eigen::Quaternionf quat() const {return this->_quat;}
+		
+		Pose inverse();
 		
 		Pose operator*(const Pose &other);
 		
-		// Variables
-		Eigen::Vector3f    pos;
-		Eigen::Quaternionf quat;
-		
 	private:
+	
+		Eigen::Vector3f _pos;
+		
+		Eigen::Quaternionf _quat;
 	
 };                                                                                                  // Semicolon needed after class declaration
 
