8254395a864eca15c929cb4b9828b49e935f4f29
Jon <jonathan.woolfrey@gmail.com>
2023-01-20 12:32:24 +1100

    Under development

--- Model/include/RigidBody.h
+++ Model/include/RigidBody.h
@@ -7,45 +7,49 @@
 #ifndef RIGIDBODY_H_
 #define RIGIDBODY_H_
 
-#include <Eigen/Geometry>                                                                           // Eigen::Affine3f
+#include <Pose.h>                                                                                   // Position & orientation
 #include <iostream>
 
 class RigidBody
 {
 	public:
+		// Constructor(s)
 		RigidBody() {}                                                                      // Empty constructory
 		
-		RigidBody(const Eigen::Isometry3f &origin,                                          // Origin relative to some other frame
-                          const Eigen::Vector3f &centreOfMass,                                      // Center of mass rleative to origin
-                          const float &_mass,                                                       // Mass (kg)
-                          const Eigen::VectorXf &momentOfInertia);                                  // Inertia (Nm^2)
+		RigidBody(const Pose            &origin,                                            // Origin relative to some other reference frame
+		          const Eigen::Vector3f &centreOfMass,                                      // Center of mass relative to own origin frame
+		          const float           &_mass,                                             // kilograms
+		          const Eigen::Matrix3f &momentOfInertia);                                  // N*m^2
 			
-		// Set Functions
-		bool set_state(const Eigen::Isometry3f &origin,
-                              const Eigen::Vector3f &linearVel,
-                              const Eigen::Vector3f &angularVel);
+		// Functions
+		        
+		Eigen::Vector3f com() const { return this->c; }
 		
-		// Get Functions
-		Eigen::Vector3f get_com() const {return this->com;}                                 // Get the position of the c.o.m. in local frame
+		Eigen::Matrix3f inertia() const { return this->I; }
 		
-		Eigen::Isometry3f get_pose() const {return this->pose;}                             // Get the pose relative to some local frame
+		Eigen::Matrix3f inertia_derivative() const { return this->Idot; }
 		
-		Eigen::Matrix3f get_inertia() const {return this->inertia;}
+		float mass() const { return this->m; }
 		
-		float get_mass() const {return this->mass;}
+		Pose pose() const { return this->P; }
+
+		void update_state(const Pose            &_pose,
+		                  const Eigen::Vector3f &linearVelocity,
+		                  const Eigen::Vector3f &angularVelocity);
 		
 	private:
-		// Kinematic Properties
-		Eigen::Isometry3f pose;                                                             // Pose of object relative to some local frame
-		Eigen::Vector3f linearVel = {0.0 , 0.0 , 0.0};
-		Eigen::Vector3f angularVel = {0.0 , 0.0 , 0.0};
+	
+		// Kinematic properties
+		Pose P;                                                                             // Pose of object relative to some local frame
+		Eigen::Vector3f v = {0.0 , 0.0 , 0.0};
+		Eigen::Vector3f w = {0.0 , 0.0 , 0.0};
 				
 		// Dynamic Properties
-		Eigen::Matrix3f inertia;                                                            // Moment of inertia at the center of mass
+		Eigen::Matrix3f H;                                                                  // Inertia in local frame
+		Eigen::Matrix3f I;                                                                  // Inertia in global frame
 		Eigen::Matrix3f Idot;                                                               // Time-derivative of inertia
-		Eigen::Vector3f com = {0.5, 0.0, 0.0};                                              // Location of center of mass in local frame
-		float mass = 1.0;                                                                   // Mass of the rigid body
-		
+		Eigen::Vector3f c = {0.5, 0.0, 0.0};                                                // Location of center of mass in local frame
+		float m = 1.0;                                                                      // Mass of the rigid body
 };                                                                                                  // Semicolon needed after a class declaration
 
 #endif
