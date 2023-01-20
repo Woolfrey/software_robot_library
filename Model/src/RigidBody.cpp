00f144ab253bba6e75b858152725bb970d88f127
Jon <jonathan.woolfrey@gmail.com>
2023-01-20 12:33:13 +1100

    In development

--- Model/src/RigidBody.cpp
+++ Model/src/RigidBody.cpp
@@ -3,34 +3,47 @@
   ////////////////////////////////////////////////////////////////////////////////////////////////////
  //                                          Constructor                                           //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-RigidBody::RigidBody(const Eigen::Isometry3f &origin,
+RigidBody::RigidBody(const Pose            &origin,
                      const Eigen::Vector3f &centreOfMass,
-                     const float &_mass,
-                     const Eigen::VectorXf &momentOfInertia):
-                     pose(origin),
-                     com(centreOfMass),
-                     mass(_mass)
-{
-	if(momentOfInertia.size() != 6)
+                     const float           &_mass,
+                     const Eigen::Matrix3f &momentOfInertia):
+                     P(origin),
+                     c(centreOfMass),
+                     m(_mass),
+                     H(momentOfInertia)
+{		
+	Eigen::Matrix3f R = this->P.quat.toRotationMatrix();                                        // Get rotation matrix
+	this->I = R*this->H*R.transpose();                                                          // Rotate to global frame
+	
+	if(this->m < 0)
 	{
-		std::cerr << "[ERROR] [RIGIDBODY] Constructor: "
-			  << "Expected 6 elements for the momentOfInertia argument. "
-			  << " Your input had " << momentOfInertia.size() << " elements." << std::endl;
-	}
-	else
-	{
-		this->inertia << momentOfInertia(0), momentOfInertia(1), momentOfInertia(2),
-				  momentOfInertia(1), momentOfInertia(3), momentOfInertia(4),
-				  momentOfInertia(2), momentOfInertia(4), momentOfInertia(5);
-	}
-				
-	if(this->mass < 0)
-	{
-		std::cerr << "[WARNING] [RIGIDBODY] Constructor: "
-                         << "Mass cannot negative. "
-                         << "Your input was " << this->mass << ". "
-			  << "Ensuring the value is positive to avoid problems..." << std::endl;
+		std::cerr << "[WARNING] [RIGID BODY] Constructor: "
+                          << "Mass argument of " << _mass << " cannot be negative." << std::endl;
 			  
-		this->mass *= -1;
+		this->m *= -1;
 	}
+}
+  ///////////////////////////////////////////////////////////////////////////////////////////////////
+ //                    Set new kinematic properties and update the dynamics                       //
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void RigidBody::update_state(const Pose            &pose,
+                             const Eigen::Vector3f &linearVelocity,
+                             const Eigen::Vector3f &angularVelocity)
+{
+	// Set new values
+	this->P = pose;
+	this->v = linearVelocity;
+	this->w = angularVelocity;
+	
+	// Rotate inertia to new global reference frame
+	Eigen::Matrix3f R = this->P.quat.toRotationMatrix();                                 
+	this->I = R*this->H*R.transpose();
+	
+	// Compute time-derivative of the inertia
+	Eigen::Matrix3f skew;
+	skew <<          0.0, -this->w(2),  this->w(1),
+	          this->w(2),         0.0, -this->w(0),
+	         -this->w(1),  this->w(0),         0.0;
+	
+	this->Idot = skew*this->I;                                                                  // d/dt(I) = s(w)*I
 }
