5861e6ec27424f09a5841466ac155b4ec4423318
Jon <jonathan.woolfrey@gmail.com>
2023-01-20 17:45:18 +1100

    Working on inverse dynamics algorithm

--- Model/include/KinematicTree.h
+++ Model/include/KinematicTree.h
@@ -19,6 +19,11 @@ class KinematicTree
 		
 		// Functions
 		
+		bool update_state(const Eigen::VectorXf &jointPosition,
+		                  const Eigen::VectorXf &jointVelocity,
+		                  const Pose &basePose,
+		                  const Eigen::Matrix<float,6,1> baseTwist);   
+		
 		Eigen::MatrixXf jacobian(const Eigen::Vector3f &point,
 		                         const std::string &branchName);
 		
@@ -30,26 +35,29 @@ class KinematicTree
 		Eigen::MatrixXf partial_derivative(const Eigen::MatrixXf &jacobian,
 		                                   const unsigned int    &jointNumber);
 		
-		Eigen::MatrixXf inertia()  const {return this->M;}
-		Eigen::MatrixXf coriolis() const {return this->C;}
+		Eigen::MatrixXf inertia()  const {return this->Mjj;}
+		Eigen::MatrixXf coriolis() const {return this->Cjj;}
 		Eigen::MatrixXf damping()  const {return this->D;}
 		Eigen::VectorXf gravity()  const {return this->g;}
-		Eigen::VectorXf nonlinear_terms() const {return (this->C+this->D)*this->qdot + this->g;}
+		Eigen::VectorXf nonlinear_terms() const {return (this->Cjj+this->D)*this->qdot + this->g;}
 		
 		// Properties
 		
 		std::vector<Branch> branch;
 		
 	private:
+	
 		unsigned int numJoints;
 	
 		// Kinematic properties
 		Eigen::VectorXf q, qdot;                                                            // Joint position and velocity
 		
 		// Dynamic properties
-		Eigen::MatrixXf M;                                                                  // Inertia matrix
-		Eigen::MatrixXf C;                                                                  // Coriolis matrix
-		Eigen::MatrixXf D;                                                                  // Damping matrix
+		Eigen::MatrixXf Mjj;                                                                // Joint inertia matrix
+		Eigen::Matrix3f Mjb;                                                                // Joint/base inertia matrix
+		Eigen::MatrixXf Cjj;                                                                // Joint Coriolis matrix
+		Eigen::Matrix3f Cjb;                                                                // Joint/base Coriolis matrix
+		Eigen::MatrixXf D;                                                                  // Joint damping matrix
 		Eigen::VectorXf g;                                                                  // Gravitational torque vector
 };                                                                                                  // Semicolon needed after class declarations
 
--- Model/src/KinematicTree.cpp
+++ Model/src/KinematicTree.cpp
@@ -1,6 +1,67 @@
 #include <KinematicTree.h>
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////
+ //                          Update the kinematic & dynamic properties                            //
+///////////////////////////////////////////////////////////////////////////////////////////////////
+bool KinematicTree::update_state(const Eigen::VectorXf &jointPosition,
+                                 const Eigen::VectorXf &jointVelocity,
+                                 const Pose &basePose,
+                                 const Eigen::Matrix<float,6,1> baseTwist)
+{
+	if(jointPosition.size() != this->numJoints or jointVelocity.size() != this->numJoints)
+	{
+		std::cerr << "[ERROR] [KINEMATIC TREE] update_state(): "
+		          << "This model has " << this->numJoints << " joints, "
+		          << "but this joint position argument had " << jointPosition.size() << " elements, "
+		          << "and the joint velocity argument had " << jointVelocity.size() << " elements." << std::endl;
+		
+		return false;
+	}
+	else
+	{
+		// Variables used in this scope
+		float m;
+		Eigen::MatrixXf J, Jdot, Jv, Jw, Jvdot, Jwdot;
+		Eigen::Matrix3f I, Idot;
+		Eigen::Vector3f com;
+		
+		
+		// Reset values
+		this->Mjj.setZero();
+		this->Mjb.setZero();
+		this->Cjj.setZero();
+		this->Cjb.setZero();
+		
+		for(int i = 0; i < this->numJoints; i++)
+		{
+			m    = this->branch[i].mass();
+			I    = this->branch[i].inertia();
+			com  = this->branch[i].pose()*this->branch[i].com();                       // Transform centre of mass to global frame
+			Idot = this->branch[i].inertia_derivative();
+			
+			// Compute joint space dynamics
+			J = jacobian(com,i+1);                                                    // Get the Jacobian to the ith centre of mass
+			Jv = J.block(0,0,3,i+1);
+			Jw = J.block(3,0,3,i+1);
+			
+			this->Mjj += m*Jv.transpose()*Jv
+			           + Jw.transpose()*I*Jw;
+			     
+
+			Jdot = time_derivative(J);
+			
+			this->Cjj += m*Jv.transpose()*Jdot.block(0,0,3,i+1)
+			           + Jw.transpose()*(I*Jdot.block(3,0,3,i+1) + Idot*Jw);
+			           
+			// Compute joint/base coupled dynamics
+			
+		}
+		
+		return true;
+	}
+}
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////
  //                           Compute the Jacobian to the given branch                            //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 Eigen::MatrixXf KinematicTree::jacobian(const Eigen::Vector3f &point,
@@ -31,15 +92,18 @@ Eigen::MatrixXf KinematicTree::jacobian(const Eigen::Vector3f &point,
 		std::cerr << "[ERROR] [KINEMATIC TREE] jacobian(): "
 		          << "Cannot compute the Jacobian up to joint " << jointNumber << " "
 		          << "as this model has only " << this->numJoints << " joints!" << std::endl;
-		
-		return J;
 	}
 	else
 	{
-		unsigned int currentBranch = jointNumber;
+/*		unsigned int i = jointNumber;
 		
-		// WORK IN PROGRESS //
-		
-		return J;
+		{
+			J.block(0,i,3,1) = this->branch[i].jointAxis().cross(point - this->branch[i].pose().pos());
+			J.block(3,i,3,1) = this->branch[i].jointAxis();
+			
+			i = this->branch[i].root();                                                 // Next along this branch
+		}*/
 	}
+	
+	return J;
 }
