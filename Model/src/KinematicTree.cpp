3adf4a3912f20db210db71053b9c312790398854
Jon <jonathan.woolfrey@gmail.com>
2023-03-24 17:50:15 +0100

    Work in progress.

--- Model/src/KinematicTree.cpp
+++ Model/src/KinematicTree.cpp
@@ -5,37 +5,103 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 KinematicTree::KinematicTree(const std::string &pathToURDF)
 {
+	std::string errorMessage = "[ERROR] [KINEMATIC TREE] Constructor: ";
+	
 	if(not std::ifstream(pathToURDF.c_str()))
 	{
-		std::string message = "[ERROR] [KINEMATIC TREE] Constructor: The file " + pathToURDF + " does not appear to exist.";
+		errorMessage += "The file " + pathToURDF + " does not appear to exist.";
 		
-		throw std::runtime_error(message);
+		throw std::runtime_error(errorMessage);
 	}
 	else
 	{
-
 		tinyxml2::XMLDocument urdf;
 		urdf.LoadFile(pathToURDF.c_str());
 		
 		tinyxml2::XMLElement* robot = urdf.FirstChildElement("robot");
-		if(robot == nullptr) throw std::runtime_error("[ERROR] [KINEMATIC TREE] Constructor: There does not appear to be a robot element in the given URDF.");
+		if(robot == nullptr)
+		{
+			errorMessage += "There does not appear to be a robot element in the URDF.";
+			
+			throw std::runtime_error(errorMessage);
+		}
 		
+		std::map<const char*, RigidBody> linkList;                                          // Create a map so we can find which link its attached to
+		
+		// Search through every link
+		for(auto link = robot->FirstChildElement("link"); link; link = link->NextSiblingElement("link"))
+		{
+			// Default values
+			float mass = 0.0;
+			Eigen::Matrix3f momentOfInertia = Eigen::Matrix3f::Zero();
+			Pose centreOfMass(Eigen::Vector3f::Zero(), Eigen::Quaternionf(1,0,0,0));
+			
+			// Get inertia properties if they exist
+			tinyxml2::XMLElement* inertial = link->NextSiblingElement("inertial");
+			if(inertial != nullptr)
+			{
+				mass = inertial->FloatAttribute("mass");
+				
+				float ixx = inertial->FloatAttribute("ixx");
+				float ixy = inertial->FloatAttribute("ixy");
+				float ixz = inertial->FloatAttribute("ixz");
+				float iyy = inertial->FloatAttribute("iyy");
+				float iyz = inertial->FloatAttribute("iyz");
+				float izz = inertial->FloatAttribute("izz");
+				
+				momentOfInertia << ixx, ixy, ixz,
+				                   ixy, iyy, iyz,
+				                   ixz, iyz, izz;
+				
+				tinyxml2::XMLElement* origin = inertial->NextSiblingElement("origin");
+				if(origin != nullptr)
+				{
+					Eigen::Vector3f xyz = char_to_vector3f(origin->Attribute("xyz"));
+					Eigen::Vector3f rpy = char_to_vector3f(origin->Attribute("rpy"));
+					
+					centreOfMass = Pose(xyz, Eigen::AngleAxisf(rpy(0),Eigen::Vector3f::UnitX())
+					                        *Eigen::AngleAxisf(rpy(1),Eigen::Vector3f::UnitY())
+					                        *Eigen::AngleAxisf(rpy(2),Eigen::Vector3f::UnitZ()));
+				}
+			}
+		
+			linkList.emplace(link->Attribute("name"), RigidBody(mass,momentOfInertia,centreOfMass)); // Add to list
+		}
+		
+		if(linkList.empty())
+		{
+			errorMessage += "Could not find any links in the given URDF.";
+			throw std::runtime_error(errorMessage);
+		}
+			
 		// Search through every joint
 		for(auto joint = robot->FirstChildElement("joint"); joint; joint = joint->NextSiblingElement("joint"))
-		{
-		
+		{	
 			const char* jointName = joint->Attribute("name");
-			const char* type      = joint->Attribute("type");
+			const char* proceedingLink = joint->FirstChildElement("child")->Attribute("link");
 			
-			std::cout << "\n" << jointName << " is a " << type << " joint.\n";
+			if(proceedingLink != nullptr)
+			{	
 			
-			tinyxml2::XMLElement* axis = joint->FirstChildElement("axis");
+				//if(auto search = linkList.find(proceedingLink))
+				//{
+					std::cout << "The " << proceedingLink << " link is attached to the " << jointName << " joint.\n";
+				//}
+				//else
+				//{
+				//	std::cout << "Could not find the attached link..." << std::endl;
+				//}
 			
-			if(axis != nullptr)
-			{
-				std::cout << "The axis is:\n" << char_to_vector3f(axis->Attribute("xyz")) << "." << std::endl;
 			}
-			
+			else
+			{
+				errorMessage += "The " + std::to_string(*jointName) + " joint has no child link. Is this correct?";
+				throw std::runtime_error(errorMessage);
+			}
+		}
+	}
+}
+
 			/*
 			// Extract joint friction information
 			tinyxml2::XMLElement* dynamics = joint->FirstChildElement("dynamics");
@@ -146,16 +212,13 @@ KinematicTree::KinematicTree(const std::string &pathToURDF)
 				this->link.emplace_back(jointObject,rigidBody);
 			}
 			*/
-		}
-		
-	}
-}
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////
  //                   Convert a char of numbers to an Eigen::Vector3f object                      //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 Eigen::Vector3f KinematicTree::char_to_vector3f(const char* character)
 {
+	// I don't know why it's 5 ¯\_(ツ)_/¯
 	if(strlen(character) == 5)
 	{
 		return Eigen::Vector3f(std::atof(&character[1]),
@@ -164,11 +227,9 @@ Eigen::Vector3f KinematicTree::char_to_vector3f(const char* character)
 	}
 	else
 	{
-		std::cout << "[ERROR] [KINEMATIC TREE] char_to_vector(): "
+		std::cout << "[ERROR] [KINEMATIC TREE] char_to_vector3f(): "
 		          << "Expected a char of length 5 but it was " << strlen(character) << "." << std::endl;
-		
-		// I don't know why it's 5 ¯\_(ツ)_/¯
-		
+
 		return Eigen::Vector3f(1,0,0);
 	}
 }
