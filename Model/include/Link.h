32f48af5595418477442649634756b514e686110
Jon <jonathan.woolfrey@gmail.com>
2023-04-12 17:02:32 +0200

    Added a name to the constructor arguments

--- Model/include/Link.h
+++ Model/include/Link.h
@@ -14,16 +14,22 @@
 class Link : public RigidBody
 {
 	public:
-		Link(const Joint     &_joint,
-		     const RigidBody &rigidBody);
+		Link(const Joint       &_joint,
+		     const RigidBody   &rigidBody,
+		     const std::string &name);
 		     
 		Joint joint;                                                                        // The joint attached to this link
 		
-		bool set_preceding_link(Link &link);
+		bool set_preceding_link(const std::shared_ptr<Link> link);
 		
-		bool add_proceeding_link(Link &link);
+		bool add_proceeding_link(const std::shared_ptr<Link> link);
+		
+		std::string name() const { return this->_name; }
 	
 	private:
+	
+		std::string _name;
+	
 		std::shared_ptr<Link> _precedingLink = nullptr;
 		
 		std::vector<std::shared_ptr<Link>> _proceedingLink = {};
