fd8bbeebf9decdf512adbdf081ccdb88e66cae3c
Jon <jonathan.woolfrey@gmail.com>
2023-04-12 17:02:48 +0200

    Added a name to the constructor arguments

--- Model/src/Link.cpp
+++ Model/src/Link.cpp
@@ -3,11 +3,13 @@
   ///////////////////////////////////////////////////////////////////////////////////////////////////
  //                                        Constructor                                            //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-Link::Link(const Joint     &_joint,
-           const RigidBody &rigidBody)
+Link::Link(const Joint       &_joint,
+           const RigidBody   &rigidBody,
+           const std::string &name)
            :
            joint(_joint),
-           RigidBody(rigidBody)
+           RigidBody(rigidBody),
+           _name(name)
 {
 	// Worker bees can leave
 	// Even drones can fly away
@@ -17,35 +19,39 @@ Link::Link(const Joint     &_joint,
   ///////////////////////////////////////////////////////////////////////////////////////////////////
  //                                    Set the preceding link                                     //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-bool Link::set_preceding_link(Link &link)
+bool Link::set_preceding_link(const std::shared_ptr<Link> link)
 {
-	try
+	if(link == nullptr)
 	{
-		this->_precedingLink = std::make_shared<Link>(std::move(link));
-		
-		return true;
-	}
-	catch(const std::exception &exception)
-	{
-		std::cout << exception.what() << std::endl;
+		std::cerr << "[ERROR] [LINK] set_preceding_link(): "
+		          << "Input appears to be a null pointer.\n";
 		
 		return false;
+	}
+	else
+	{
+		this->_precedingLink = link;                                                        // Assign the previous link
+		
+		return true;
 	}
 }
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////
  //                          Add this link to the list of proceeding links                        //
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-bool Link::add_proceeding_link(Link &link)
+bool Link::add_proceeding_link(const std::shared_ptr<Link> link)
 {
-	try
+	if(link == nullptr)
 	{
-		this->_proceedingLink.push_back(std::make_shared<Link>(std::move(link)));
-		return true;
-	}
-	catch(const std::exception &exception)
-	{
-		std::cout << exception.what() << std::endl;
+		std::cerr << "[ERROR] [LINK] set_proceeding_link(): "
+		          << "Input appears to be a null pointer.\n";
+		
 		return false;
+	}
+	else
+	{
+		this->_proceedingLink.push_back(link);                                              // Add it to the list
+		
+		return true;
 	}
 }
