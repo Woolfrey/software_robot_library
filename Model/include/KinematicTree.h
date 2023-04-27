e41bafc3cc3351e3c8ee01348a0750fabed26b3a
Jon <jonathan.woolfrey@gmail.com>
2023-04-27 11:27:25 +0200

    Started on functionality for forward kinematics

--- Model/include/KinematicTree.h
+++ Model/include/KinematicTree.h
@@ -18,7 +18,20 @@ class KinematicTree
 	public:
 		KinematicTree(const std::string &pathToURDF);                                       // Constructor from URDF
 
+		bool update_state(const Eigen::VectorXf &jointPos,
+		                  const Eigen::VectorXf &jointVel)
+		{
+			return update_state(jointPos,jointVel,this->_basePose,Eigen::VectorXf::Zero(6));
+		}
+		                  
+		bool update_state(const Eigen::VectorXf          &jointPos,
+		                  const Eigen::VectorXf          &jointVel,
+		                  const Pose                     &basePose,
+		                  const Eigen::Matrix<float,6,1> &baseTwist);
+		                  
 	private:
+		
+		int numJoints;
 
 		std::map<std::string, Joint> jointList;
 		
@@ -26,6 +39,11 @@ class KinematicTree
 		
 		Eigen::Vector3f char_to_vector3f(const char* character);                            // Used to parse urdf properties
 		
+		Link* baseLink;                                                                     // Pointer to the base of the tree
+		
+		Pose _basePose;
+
+		Eigen::Matrix<float,6,1> _baseTwist;
 };                                                                                                  // Semicolon needed after class declarations
 
 #endif
