b38b848037d09796eb209e2d7639166484c820e8
Jon <jonathan.woolfrey@gmail.com>
2023-04-29 18:45:03 +0200

    Added methods for pointers to adjacent links in the kinematic tree

--- Model/include/Link.h
+++ Model/include/Link.h
@@ -21,17 +21,29 @@ class Link : public RigidBody
 		_name(name),
 		RigidBody(rigidBody) {}
 		
-		bool merge(const Link *other);                                                      // Merge the properties of the other link with this one
+		bool merge(Link *other);                                                            // Merge the properties of the other link with this one
 		
 		bool attach_joint(Joint *joint);                                                    // Specify a pointer to the preceding joint
 		
+		bool set_previous_link(Link *link);
+		
+		bool add_next_link(Link *link);
+		
 		Joint* attached_joint() const { return this->attachedJoint; }                       // Get the pointer to the joint
+		
+		Link* previous_link() const { return this->previousLink; }
+		
+		std::vector<Link*> next_links() const { return this->nextLinks; }
 		
 		std::string name() const { return this->_name; }                                    // Return the name of this link
 		     
 	private:
 		
 		Joint *attachedJoint = nullptr;                                                     // Pointer to the attached joint
+		
+		Link *previousLink = nullptr;                                                       // Pointer to previous link in the kinematic chain
+		
+		std::vector<Link*> nextLinks {};                                                    // Pointer to next links in the kinematic chain
 		
 		std::string _name;
 };                                                                                                  // Semicolon needed at the end of class declaration
