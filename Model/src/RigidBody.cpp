daa9174358e89533a6b01e55ece55771c4ff5d8b
Jon <jonathan.woolfrey@gmail.com>
2023-07-16 18:41:18 +0200

    Minor changes to formatting for readability

--- Model/src/RigidBody.cpp
+++ Model/src/RigidBody.cpp
@@ -9,14 +9,17 @@ RigidBody::RigidBody(const float           &mass,
                      const Eigen::Vector3f &centerOfMass)
                      :
                      _mass(mass),
+                     _inertia(inertia),
                      _centerOfMass(centerOfMass)
 {	
 	if(mass < 0.0)
 	{
-		throw std::invalid_argument("[ERROR] [RIGID BODY] Constructor: Mass argument was " + std::to_string(mass) + " but cannot be negative.");
+		throw std::invalid_argument("[ERROR] [RIGID BODY] Constructor: Mass argument was "
+		                           + std::to_string(mass) + " but cannot be negative.");
 	}
 	else if((inertia - inertia.transpose()).norm() > 1e-04)
 	{
-		throw std::invalid_argument("[ERROR] [RIGID BODY] Constructor: Moment of inertia does not appear to be symmetric.");
+		throw std::invalid_argument("[ERROR] [RIGID BODY] Constructor: "
+		                            "Moment of inertia does not appear to be symmetric.");
 	}
 }
