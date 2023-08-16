869e9c47f265e757646a5c6bcc93955f648c7bcd
Jon <jonathan.woolfrey@gmail.com>
2023-08-16 13:21:15 +0200

    Test code now being placed in respective directory

--- Test/pose_test.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-#include <Pose.h>
-
-
-int main(int argc, char **argv)
-{
-	return 0;                                                                                   // No problems with main
-}
