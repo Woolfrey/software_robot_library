1ba9f1e14086d9375ea0fde7e30bf66eee349f8d
Jon <jonathan.woolfrey@gmail.com>
2023-08-17 17:26:35 +0200

    Cleaned up code

--- Trajectory/include/CubicSpline.h
+++ Trajectory/include/CubicSpline.h
@@ -32,7 +32,7 @@ class CubicSpline
 	private:
 	
 		// Private properties
-		vector<Polynomial> spline;                                                          // Array of cubic polynomials
+		vector<Polynomial<DataType>> spline;                                                 // Array of cubic polynomials
 		
 		vector<DataType> _time;                                                             // Array of times to reach each waypoint
 		
@@ -43,7 +43,7 @@ class CubicSpline
 };                                                                                                  // Semicolon needed after class declaration
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
- //                                           Constructor                                          //
+ //                                         Constructor                                            //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 template <class DataType>
 CubicSpline<DataType>::CubicSpline(const vector<Vector<DataType,Dynamic>> &waypoint,
@@ -76,33 +76,67 @@ CubicSpline<DataType>::CubicSpline(const vector<Vector<DataType,Dynamic>> &waypo
 		{
 			throw logic_error("[ERROR] [CUBIC SPLINE] Constructor: "
 			                  "Time of " + to_string(time[i]) + " for waypoint " + to_string(i) + " "
-			                  "is greater than time of " + to_string(time[i])) + " for waypoint "
+			                  "is greater than time of " + to_string(time[i]) + " for waypoint "
 			                  + to_string(i) + ". The arrow of time only moves in one direction.");
 		}
 	}
 	
-	// Compute the velocities for each of the waypoints
-	Vector<DataType,Dynamic> velocity::zero(this->numPoints);
+	// Compute the velocities for each of the waypoints. They are governed by the relationship:
+	// A*xdot = B*x
+	
+	Matrix<DataType,Dynamic,Dynamic> A;
+	A.resize(this->_numPoints,this->_numPoints);
+	A.setZero();
+	
+	Matrix<DataType,Dynamic,Dynamic> B = A;
+	
+	A(0,0) = 1;
+	for(int i = 1; i < this->_numPoints-1; i++)
+	{
+		DataType dt1 = time[i-1];
+		DataType dt2 = time[i];
+		
+		A(i-1,i) = 1/dt1;
+		A(i,i)   = 2*(1/dt1 + 1/dt2);
+		A(i,i+1) = 1/dt2;
+	
+		B(i-1,i) = -3/(dt1*dt1);
+		B(i,i)   =  3(1/(dt1*dt1) - 1/(dt2*dt2));
+		B(i,i+1) =  3/(dt2*dt2);
+	}
+	A(this->_numPoints, this->_numPoints) = 1;
+	
+	Matrix<DataType,Dynamic,Dynamic> C = A.partialPivLu().solve(B);                             // Makes calcs a little easier
+	
+	vector<Vector<DataType,Dynamic>> velocity; velocity.resize(this->_numPoints);               // Array of velocities for all dimensions, waypoints
+	
+	for(int i = 0; i < this->_numPoints; i++)
+	{
+		velocity[i].resize(this->_dimensions);                                              // Resize the Eigen::Vector object within the std::vector object
+		
+		for(int j = 0; j < this->_dimensions; j++)
+		{
+			velocity[i][j] = 0.0;
+			
+			// Note: waypoints for a single dimension are stored along the rows,
+			//       so we need to project the rows of the C matrix on to the rows of
+			//       the waypoint array
+			for(int k = 0; k < this->_dimensions; k++) velocity[i][j] += C(j,k)*waypoint[i][k];
+		}
+	}
 	
 	// Now create n-1 cubic polynomial splines
 	for(int i = 0; i < this->numPoints-1; i++)
 	{
 		try
 		{
-			this->spline.emplace_back(waypoint[i],
-			                          waypoint[i+1],
-			                          velocity[i],
-			                          velocity[i+1],
-			                          time[i],
-			                          time[i+1],
-			                          3)
+			this->spline.emplace_back(waypoint[i], waypoint[i+1], velocity[i], velocity[i+1], time[i], time[i+1], 3);
 		}
 		catch(const exception &exception)
 		{
 			cout << exception.what() << endl;
 			
-			throw runtime_error("[ERROR] [CUBIC SPLINE] Constructor: "
-			                    "Could not generate the spline.");
+			throw runtime_error("[ERROR] [CUBIC SPLINE] Constructor: Could not generate the spline.");
 		}
 	}
 }
@@ -127,7 +161,7 @@ bool CubicSpline<DataType>::get_state(Vector<DataType,Dynamic> &pos,
 			  
 		return false;
 	}
-	else if(pos.size() != this->dimensions)
+	else if(pos.size() != this->_dimensions)
 	{
 		cerr << "[ERROR] [CUBIC SPLINE] get_state() : "
 	             << "This spline has " << this->dimensions << " dimensions, "
