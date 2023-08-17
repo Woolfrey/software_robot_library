9dc86007314e4061ff68a8a76aa25b0fe9c01147
Jon <jonathan.woolfrey@gmail.com>
2023-08-17 09:27:13 +0200

    Elaborating on methods inside header file

--- Trajectory/include/TrapezoidalVelocity.h
+++ Trajectory/include/TrapezoidalVelocity.h
@@ -8,29 +8,63 @@
 #define TRAPEZOIDAL_VELOCITY_H_
 
 #include <Eigen/Geometry>                                                                           // Eigen::Vector, Eigen::Matrix, Eigen::Quaternion
-#include "TrajectoryBase.h"
+#include <TrajectoryBase.h>
 
-class TrapezoidalVelocity : public TrajectoryBase
+using namespace Eigen;                                                                              // Eigen::Vector
+
+template <class DataType>
+class TrapezoidalVelocity : public TrajectoryBase<DataType>
 {
 	public:
 		// Empty constructor
 		TrapezoidalVelocity() {}
 		
 		// Constructor for translations
-		TrapezoidalVelocity(const Eigen::VectorXf &startPoint,
-                                    const Eigen::VectorXf &endPoint,
-                                    const float           &velocity,
-                                    const float           &acceleration);
+		TrapezoidalVelocity(const Vector<DataType,Dynamic> &startPoint,
+                                    const Vector<DataType,Dynamic> &endPoint,
+                                    const DataType                 &maxVel,
+                                    const DataType                 &maxAcc);
 		
 		// Get state for translations
-		bool get_state(Eigen::VectorXf &pos,
-		               Eigen::VectorXf &vel,
-		               Eigen::VectorXf &acc,
-		               const float &time);			
+		bool get_state(Vector<DataType,Dynamic> &pos,
+		               Vector<DataType,Dynamic> &vel,
+		               Vector<DataType,Dynamic> &acc,
+		               const DataType           &time);			
 				
 	private:
-		float maxVel, maxAcc;
+		DataType _maxVel,
+		         _maxAcc;
 		
 };                                                                                                  // Semicolon needed after class declaration
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+ //                                            Constructor                                         //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class DataType>
+TrapezoidalVelocity<DataType>::TrapezoidalVelocity(const Vector<DataType,Dynamic> &startPoint,
+                                                   const Vector<DataType,Dynamic> &endPoint,
+                                                   const DataType                 &maxVel,
+                                                   const DataType                 &maxAcc)
+                                                   :
+                                                   TrajectoryBase(_startTime,_endTime,dimensions),
+                                                   maxVel(velocity),
+                                                   maxAcc(acceleration)
+                                         
+{
+	
+}
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////
+ //                               Get the state for the given time                                //
+///////////////////////////////////////////////////////////////////////////////////////////////////
+template <class DataType> inline
+bool TrapezoidalVelocity<DataType>::get_state(Vector<DataType,Dynamic> &pos,
+                                              Vector<DataType,Dynamic> &vel,
+                                              Vector<DataType,Dynamic> &acc,
+                                              const DataType           &time)
+{
+    return true;
+}
+
 
 #endif
