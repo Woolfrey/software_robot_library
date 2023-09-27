591c1fb60b408529a035d1c585a6fb27b8cf16ae
Jon <jonathan.woolfrey@gmail.com>
2023-09-27 09:53:51 +0200

    Saving work in progress

--- Trajectory/include/TrapezoidalVelocity.h
+++ Trajectory/include/TrapezoidalVelocity.h
@@ -23,7 +23,8 @@ class TrapezoidalVelocity : public TrajectoryBase<DataType>
 		TrapezoidalVelocity(const Vector<DataType,Dynamic> &startPoint,
                                     const Vector<DataType,Dynamic> &endPoint,
                                     const DataType                 &maxVel,
-                                    const DataType                 &maxAcc);
+                                    const DataType                 &maxAcc,
+                                    const DataType                 &startTime;
 		
 		// Get state for translations
 		bool get_state(Vector<DataType,Dynamic> &pos,
@@ -35,7 +36,11 @@ class TrapezoidalVelocity : public TrajectoryBase<DataType>
 		DataType _maxVel;                                                                   ///< Maximum speed
 		DataType _maxAccel;                                                                 ///< Maximum acceleration
 		DataType _dt;                                                                       ///< Ration of speed over acceleration
+		DataType _t1;
+		DataType _t2;
 		
+		Vector<DataType,Dynamic> p1;
+		Vector<DataType,Dynamic> p2;
 };                                                                                                  // Semicolon needed after class declaration
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -45,7 +50,12 @@ template <class DataType>
 TrapezoidalVelocity<DataType>::TrapezoidalVelocity(const Vector<DataType,Dynamic> &startPoint,
                                                    const Vector<DataType,Dynamic> &endPoint,
                                                    const DataType                 &maxVel,
-                                                   const DataType                 &maxAcc)                          
+                                                   const DataType                 &maxAcc,
+                                                   const DataType                 &startTime)
+                                                   :
+                                                   _maxVel(maxVel),
+                                                   _maxAccel(maxAccel),
+                                                   _startTime(startTime);              
 {
 	if(startPoint.size() != endPoint.size())
 	{
@@ -61,6 +71,21 @@ TrapezoidalVelocity<DataType>::TrapezoidalVelocity(const Vector<DataType,Dynamic
 		                       + to_string(maxVel) + " and " + to_string(maxAcc) +
 		                       " but must be positive.");
 	}
+	
+	// Ensure the maximum velocity is not too large
+	for(int i = 0; i < startPoint.size(); i++)
+	{
+		DataType temp = sqrt(maxAcc*abs(endPoint(i) - startPoint(i)));
+		
+		if( this->_maxVel >= temp)
+		{
+			cout << "[WARNING] [TRAPEZOIDAL VELOCITY] Constructor: "
+			     << "Maximum velocity too high for the distance travelled. "
+			     << "Overriding value.\n";
+			     
+			this->_maxVel = temp;
+		}
+	}
 }
 
   ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -72,7 +97,27 @@ bool TrapezoidalVelocity<DataType>::get_state(Vector<DataType,Dynamic> &pos,
                                               Vector<DataType,Dynamic> &acc,
                                               const DataType           &time)
 {
-    return true;
+	// Make sure the inputs are sound
+	if(pos.size() != this->dim)
+	{
+		cerr << "[ERROR] [TRAPEZOIDAL VELOCITY] get_state(): "
+		     << "This trajectory object has " << this->dim << " dimensions, "
+		     << "but your input argument had " << pos.size() << " elements.\n";
+		     
+		return false;
+	}
+	else if(pose.size() != vel.size() or vel.size() != acc.size())
+	{
+		cerr << "[ERROR] [TRAPEZOIDAL VELOCITY] get_state(): "
+		     << "Dimensions of arguments do not match. "
+		     << "Position argument had " << pos.size() << " elements, "
+		     << "velocity argument had " << vel.size() << " elements, and "
+		     << "acceleration argument had + " << acc.size() << " elements.\n";
+		     
+		return false;
+	}
+	
+	return true;
 }
 
 
