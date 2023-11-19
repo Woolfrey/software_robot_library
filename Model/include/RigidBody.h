4b925889d7c092040359201537fb9dd2cf18a5e7
Jon <jonathan.woolfrey@gmail.com>
2023-11-19 16:37:32 +0100

    Changes to naming to reflect better coding practice

--- Model/include/RigidBody.h
+++ Model/include/RigidBody.h
@@ -30,40 +30,40 @@ class RigidBody
 		 * @param inertia 3x3 matrix specifying the moment of inertia (kg*m^2)
 		 * @param centerOfMass The position of the center of mass relative to a reference frame on the object.
 		 */
-		RigidBody(const string               &name,
-			  const DataType             &mass,
-		          const Matrix<DataType,3,3> &inertia,
-		          const Vector<DataType,3>   &centerOfMass);
+		RigidBody(const std::string                 &name,
+			  const DataType                    &mass,
+		          const Eigen::Matrix<DataType,3,3> &inertia,
+		          const Eigen::Vector<DataType,3>   &centerOfMass);
 		      
 		/**
 		 * @return Returns the mass of this object.
 		 */
-		DataType mass()                const { return this->_mass; }
+		DataType mass() const { return this->_mass; }
 		
 		/**
 		 * @return Returns the moment of inertia for this object.
 		 */
-		Matrix<DataType,3,3> inertia() const { return this->_inertia; }
+		Eigen::Matrix<DataType,3,3> inertia() const { return this->_inertia; }
 		
 		/**
 		 * @return The pose of this object in some global frame.
 		 */
-		Pose<DataType> pose()          const { return this->_pose; }
+		Pose<DataType> pose() const { return this->_pose; }
 		
 		/**
 		 * @return The name of this object.
 		 */
-		string name()                  const { return this->_name; }
+		std::string name() const { return this->_name; }
 		
 		/**
 		 * @return The center of mass for this object in its local reference frame.
 		 */
-		Vector<DataType,3> com()       const { return this->_centerOfMass; }                // Get the center of mass
+		Eigen::Vector<DataType,3> center_of_mass() const { return this->_centerOfMass; }    // Get the center of mass
 		
 		/**
 		 * @return The linear and angular velocity of this object.
 		 */
-		Vector<DataType,6> twist()     const { return this->_twist; }
+		Eigen::Vector<DataType,6> twist() const { return this->_twist; }
 		
 		/**
 		 * Combines the inertial properties of another rigid body with this one.
@@ -78,25 +78,28 @@ class RigidBody
 		 * @param pose The pose of this object in a global reference frame.
 		 * @param twist The linear and angular velocity of this object.
 		 */              
-		void update_state(const Pose<DataType> &pose, const Vector<DataType,6> &twist)
-		{
-			this->_pose  = pose;
-			this->_twist = twist;
-		}                  
+		void update_state(const Pose<DataType> &pose,
+		                  const Eigen::Vector<DataType,6> &twist);
 		
-	protected:
+	private:
 	          
 		DataType _mass = 0.0;                                                               ///< How heavy the object is (kg)                                             
+	
+		Eigen::Matrix<DataType,3,3> _inertia = Eigen::Matrix<DataType,3,3>::Zero();         ///< Inertia in GLOBAL reference frame
 		
-		Matrix<DataType,3,3> _inertia = Matrix<DataType,3,3>::Zero();                       ///< Moment of inertia (kg*m^2)
+		Eigen::Matrix<DataType,3,3> _inertiaDerivative = Eigen::Matrix<DataType,3,3>::Zero(); ///< Time derivative of the moment of inertia
 		
-		Pose<DataType> _pose;                                                               ///< Pose of the object in global reference frame
+		Eigen::Matrix<DataType,3,3> _localInertia = Eigen::Matrix<DataType,3,3>::Zero();    ///< Moment of inertia (kg*m^2) in LOCAL frame
+				
+		Eigen::Vector<DataType,3>   _centerOfMass = {0,0,0};                                ///< Location for the center of mass in GLOBAL frame
 		
-		string _name = "unnamed";                                                           ///< Unique identifier for this object.
+		Eigen::Vector<DataType,3>   _localCenterOfMass = {0,0,0};                           ///< Location for center of mass in LOCAL frame
 		
-		Vector<DataType,3> _centerOfMass = {0,0,0};                                         ///< Location for the center of mass in its LOCAL frame
+		Eigen::Vector<DataType,6>   _twist = Vector<DataType,6>::Zero();                    ///< Linear and angular velocity of the object.
+
+		Pose<DataType> _pose;                                                               ///< Pose of the object in GLOBAL reference frame
 		
-		Vector<DataType,6> _twist = Vector<DataType,6>::Zero();                             ///< Linear and angular velocity of the object.
+		std::string _name = "unnamed";                                                      ///< Unique identifier for this object.
 		
 };                                                                                                  // Semicolon needed after a class declaration
 
@@ -104,25 +107,25 @@ class RigidBody
  //                                          Constructor                                           //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 template <class DataType>
-RigidBody<DataType>::RigidBody(const string               &name,
-			       const DataType             &mass,
-                               const Matrix<DataType,3,3> &inertia,
-                               const Vector<DataType,3>   &centerOfMass)
+RigidBody<DataType>::RigidBody(const std::string                 &name,
+			       const DataType                    &mass,
+                               const Eigen::Matrix<DataType,3,3> &inertia,
+                               const Eigen::Vector<DataType,3>   &centerOfMass)
                                :
                                _name(name),
                                _mass(mass),
-                               _inertia(inertia),
-                               _centerOfMass(centerOfMass)
+                               _localInertia(inertia),
+                               _localCenterOfMass(centerOfMass)
 {	
 	if(mass < 0.0)
 	{
-		throw invalid_argument("[ERROR] [RIGID BODY] Constructor: "
-		                       "Mass argument was " + to_string(mass) + " but cannot be negative.");
+		throw std::invalid_argument("[ERROR] [RIGID BODY] Constructor: "
+		                            "Mass argument was " + std::to_string(mass) + " but cannot be negative.");
 	}
 	else if((inertia - inertia.transpose()).norm() > 1e-04)
 	{
-		throw invalid_argument("[ERROR] [RIGID BODY] Constructor: "
-		                       "Moment of inertia does not appear to be symmetric.");
+		throw std::invalid_argument("[ERROR] [RIGID BODY] Constructor: "
+		                            "Moment of inertia does not appear to be symmetric.");
 	}
 }
 
@@ -135,15 +138,38 @@ void RigidBody<DataType>::combine_inertia(const RigidBody<DataType> &other,
 {
 	this->_mass += other.mass();                                                                // Add the masses together
 
-	Matrix<DataType,3,3> R = pose.rotation();                                                   // Get the rotation matrix
+	Eigen::Matrix<DataType,3,3> R = pose.rotation();                                            // Get the rotation matrix
 	
-	Vector<DataType,3> t = pose.translation() + R*other.com();                                  // Transform the center of mass for the other link to THIS coordinate frame
+	Eigen::Vector<DataType,3> t = pose.translation() + R*other.center_of_mass();                // Transform the center of mass for the other link to THIS coordinate frame
 	
-	Matrix<DataType,3,3> S; S <<   0 , -t(2),  t(1), 
-	                             t(2),    0 , -t(0),
-	                            -t(1),  t(0),    0;                                             // As a skew-symmetric matrix
+	Eigen::Matrix<DataType,3,3> S; S <<   0 , -t(2),  t(1), 
+	                                    t(2),    0 , -t(0),
+	                                   -t(1),  t(0),    0;                                      // As a skew-symmetric matrix
 	
-	this->_inertia += R*other.inertia()*R.transpose() - other.mass()*S*S;                       // From the parallel axis theorem
+	this->_localInertia += R*other.inertia()*R.transpose() - other.mass()*S*S;                  // From the parallel axis theorem
 }
 
+  ///////////////////////////////////////////////////////////////////////////////////////////////////
+ //                     Update the pose and velocity of this rigid body                           //
+///////////////////////////////////////////////////////////////////////////////////////////////////
+template <class DataType> inline
+void RigidBody<DataType>::update_state(const Pose<DataType> &pose,
+                                       const Eigen::Vector<DataType,6> &twist)
+{
+	this->_pose  = pose;                                                                        // Update the pose of this object
+	
+	this->_centerOfMass = this->_pose*this->_localCenterOfMass;                                 // Point transformation to global frame
+	
+	this->_twist = twist;                                                                       // Update the linear & angular velocity
+	
+	Eigen::Matrix<DataType,3,3> R = pose.rotation();                                            // Get the rotation component as SE(3)
+	
+	this->_inertia = R*this->_localInertia*R.transpose();                                       // Rotate inertia to global reference frame
+
+	Eigen::Matrix<DataType,3,3> S; S <<        0 , -twist(5),  twist(4),
+	                                     twist(5),        0 , -twist(3),
+	                                    -twist(4),  twist(3),        0;                         // Angular velocity as skew-symmetric matrix
+
+	this->_inertiaDerivative = S*this->_inertia;                                                // Time derivative of the angular inertia
+}
 #endif
