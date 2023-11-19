ff95db6d4f941d1b3dd506bbbe728c6158abeee3
Jon <jonathan.woolfrey@gmail.com>
2023-11-19 16:38:25 +0100

    Saving work in progress; needs overhaul

--- Trajectory/include/CartesianTrajectory.h
+++ Trajectory/include/CartesianTrajectory.h
@@ -1,6 +1,6 @@
     ////////////////////////////////////////////////////////////////////////////////////////////////////
    //                                                                                                //
-  //                      A minimum acceleration trajectory in Cartesian space                      //
+  //           A class for defining trajectories in SE(3), i.e. position & orientation              //
  //                                                                                                //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -10,26 +10,43 @@
 #include <CubicSpline.h>                                                                            // Custom trajectory class
 #include <Pose.h>
 
-using namespace Eigen;                                                                              // Eigen::Vector
-using namespace std;                                                                                // std::vector, std::logic_error
+template <typename DataType>
+struct CartesianState
+{
+	Pose<DataType>            pose;
+	Eigen::Vector<DataType,6> twist;
+	Eigen::Vector<DataType,6> acceleration;
+};                                                                                                  // Semicolon needed after class declaration
 
 template <class DataType>
 class CartesianTrajectory
 {
 	public:
-	
-		// Constructors
-		CartesianTrajectory();
+		/**
+		 * Constructor.
+		 * @param poses The poses that define the waypoints on the trajectory.
+		 * @param times The time at which to pass through each pose.
+		 */
+		CartesianTrajectory(const std::vector<Pose<DataType>>  &poses,
+		                    const std::vector<DataType>        &times);
 		
-		CartesianTrajectory(const vector<Pose<DataType>>  &waypoint,
-		                    const vector<DataType>        &time);
-		             
-		// Methods       
-		bool get_state(Pose<DataType>      &pose,
-			       Vector<DataType,6>  &vel,
-			       Vector<DataType,6>  &acc,
-			       const DataType      &time);
-	
+		/**
+		 * Query the pose for the given input time.
+		 * @param time The point at which to evaluate the pose.
+		 * @return The pose (position & orientation) as RobotLibrary object.
+		 */
+		Pose<DataType> query_pose(const DataType &time)
+		{
+			return query_state(time).pose;                                              // Too easy lol ᕙ(▀̿̿ĺ̯̿̿▀̿ ̿) ᕗ
+		}
+		
+		/**
+		 * Query the state (pose, velocity, acceleration) for the given time.
+		 * @param time The point at which to evaluate the state.
+		 * @return The state as a CartesianState data structure.
+		 */
+		CartesianState<DataType> query_state(const DataType &time);
+		
 	private:
 	
 		unsigned int numPoints;
@@ -43,20 +60,20 @@ class CartesianTrajectory
  //                                        Constructor                                             //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 template <class DataType>
-CartesianTrajectory<DataType>::CartesianTrajectory(const vector<Pose<DataType>> &waypoint,
-                                                   const vector<DataType>       &time)
+CartesianTrajectory<DataType>::CartesianTrajectory(const std::vector<Pose<DataType>> &poses,
+                                                   const std::vector<DataType>       &times)
                                                    :
                                                    numPoints(waypoint.size())
 {
 	if(waypoint.size() != time.size())
 	{
-		throw logic_error("[ERROR] [CARTESIAN TRAJECTORY] Constructor: "
-                                  "Length of input arguments do not match. "
-                                  "There were " + to_string(waypoint.size()) + " waypoints "
-                                  "and " + to_string(time.size()) + " times.");
+		throw std::invalid_argument("[ERROR] [CARTESIAN TRAJECTORY] Constructor: "
+                                            "Length of input arguments do not match. "
+                                            "There were " + std::to_string(waypoint.size()) + " waypoints "
+                                            "and " + std::to_string(time.size()) + " times.");
 	}
 
-	vector<Vector<DataType,6>> point;                                                           // 3 for translation and 3 for orientation
+	std::vector<Eigen::Vector<DataType,6>> point;                                               // 3 for translation and 3 for orientation
 	
 	point.resize(waypoint.size());                                                              // Resize object for the number of waypoints
 	
@@ -64,13 +81,13 @@ CartesianTrajectory<DataType>::CartesianTrajectory(const vector<Pose<DataType>> 
 	{
 		point[i].head(3) = waypoint[i].position();                                          // Grab the translation component directly
 		
-		Vector<DataType,3> epsilon = waypoint[i].quaternion().vec();                        // Get the vector part of the quaternion
+		Eigen::Vector<DataType,3> epsilon = waypoint[i].quaternion().vec();                 // Get the vector part of the quaternion
 		
 		DataType norm = epsilon.norm();                                                     // Magnitude of the vector                                     
 		
 		DataType angle = 2*asin(norm);                                                      // Extract angle embedded in quaternion
 		
-		if(abs(angle) < 1e-04) point[i].tail(3) = Vector<DataType,3>::Zero();               // Very small; zero rotation
+		if(abs(angle) < 1e-04) point[i].tail(3) = Eigen::Vector<DataType,3>::Zero();        // Very small; zero rotation
 		else                   point[i].tail(3) = angle*(epsilon/norm);                     // Angle*axis
 	}
 	
@@ -80,10 +97,10 @@ CartesianTrajectory<DataType>::CartesianTrajectory(const vector<Pose<DataType>> 
 	}
 	catch(const exception &exception)
 	{
-		cout << exception.what() << endl;
+		std::cerr << exception.what() << std::endl;                                         // Use std::endl in conjunction with std::cerr?
 		
-		throw runtime_error("[ERROR] [CARTESIAN TRAJECTORY] Constructor: "
-		                    "Could not generate a trajectory.");
+		throw std::runtime_error("[ERROR] [CARTESIAN TRAJECTORY] Constructor: "
+		                         "Could not generate a trajectory.");
 	}
 }
 
@@ -91,36 +108,30 @@ CartesianTrajectory<DataType>::CartesianTrajectory(const vector<Pose<DataType>> 
  //                         Get the desired state for the given time                               //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 template <class DataType> inline
-bool CartesianTrajectory<DataType>::get_state(Pose<DataType>      &pose,
-                                              Vector<DataType,6>  &vel,
-                                              Vector<DataType,6>  &acc,
-                                              const DataType      &time)
+CartesianState<DataType> CartesianTrajectory<DataType>::query_state(const DataType &time)
 {
-	Vector<DataType,6> pos;                                                                     // Temporary placeholder
+	auto &[position, velocity, acceleration] = this->_trajectory.query_state(time);             // Get the state for the given time
 	
-	if(this->trajectory.get_state(pos,vel,acc,time))
-	{
-		DataType angle = pos.tail(3).norm();                                                // Last 3 elements are angle x axis
-		
-		if(abs(angle) < 1e-04) pose = (pos.head(3), Quaternion<DataType>(1,0,0,0));         // Assume zero rotation
-		else
-		{
-			Vector<DataType,3> axis = pos.tail(3).normalized();
-			
-			pose = Pose<DataType>(pos.head(3), Quaternion<DataType>(cos(0.5*angle),
-                                                                                sin(0.5*angle)*axis(0),
-			                                                        sin(0.5*angle)*axis(1),
-			                                                        sin(0.5*angle)*axis(2)));
-		}
-		
-		return true;
-	}
+	// Convert the position vector to a pose object
+	
+	DataType angle = position.tail(3).norm();                                                   // Norm of the angle*axis component
+	
+	Pose pose;                                                                                  // We need to compute this
+	
+	if(abs(angle) < 1e-04) pose = Pose(position.head(3), Eigen::Quaternion<DataType>(1,0,0,0)); // Assume zero rotation
 	else
 	{
-		cerr << "[ERROR] [CARTESIAN TRAJECTORY] get_state(): "
-		     << "Unable to get the state for some reason.\n";
-
-		return false;
+		Eigen::Vector<DataType,3> axis = position.tail(3).normalized();
+		
+		pose = Pose<DataType>(position.head(3), Eigen::Quaternion<DataType>(cos(0.5*angle),
+		                                                                    sin(0.5*angle)*axis(0),
+		                                                                    sin(0.5*angle)*axis(1),
+		                                                                    sin(0.5*angle)*axis(2)));
 	}
+	
+	CartesianState<DataType> returnValue = {pose, velocity, acceleration};                      // Put them together in data structure
+	
+	return returnValue;
 }
+
 #endif
