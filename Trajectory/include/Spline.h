7554b2bb78ddebcde803c07fd0a5f56771f0b520
Jon <jonathan.woolfrey@gmail.com>
2023-11-26 17:40:58 +0100

    Position interpolated correctly; problem with velocities

--- Trajectory/include/Spline.h
+++ Trajectory/include/Spline.h
@@ -16,6 +16,20 @@
  class Spline : public Waypoints<DataType, Polynomial<DataType>>
  {
  	public:
+		
+		/**
+		 * Basic constructor for 3rd order polynomials (i.e. cubic spline) with zero start and end velocities.
+		 * @param waypoint The points which to pass through in the trajectory.
+		 * @param times The time at which to pass each point.
+		 */
+		Spline(const std::vector<Eigen::Vector<DataType,Eigen::Dynamic>> &waypoints,
+		       const std::vector<DataType> &times)
+		:
+		Spline(waypoints,times,
+		       Eigen::Vector<DataType,Eigen::Dynamic>::Zero(waypoints.front().size()),
+		       Eigen::Vector<DataType,Eigen::Dynamic>::Zero(waypoints.back().size()),
+		       3) {}
+ 		
  		/**
  		 * Constructor where start and end velocities are assumed zero.
  		 * @param waypoints An array of Eigen::Vector objects defining waypoints on the trajectory.
@@ -25,29 +39,22 @@
  		       const std::vector<DataType> &times,
  		       const unsigned int &polynomialOrder)
 		:
-		Spline(waypoints, times, polynomialOrder,
+		Spline(waypoints,time,
 		       Eigen::Vector<DataType,Eigen::Dynamic>::Zero(waypoints.front().size()),
-		       Eigen::Vector<DataType,Eigen::Dynamic>::Zero(waypoints.back().size())) {}
+		       Eigen::Vector<DataType,Eigen::Dynamic>::Zero(waypoints.back().size()),
+		       polynomialOrder) {}
 		 
  		/**
- 		 * Constructor with start and end velocities specified.
+ 		 * Full constructor which specifies the polynomial order, start and end velocities.
+ 		 *
  		 *
  		 */
  		Spline(const std::vector<Eigen::Vector<DataType,Eigen::Dynamic>> &waypoints,
  		       const std::vector<DataType> &times,
-		       const unsigned int &polynomialOrder,
- 		       const Eigen::Vector<DataType,Eigen::Dynamic> &startVelocity,
- 		       const Eigen::Vector<DataType,Eigen::Dynamic> &endVelocity);
+		       const Eigen::Vector<DataType,Eigen::Dynamic> &startVelocity,
+		       const Eigen::Vector<DataType,Eigen::Dynamic> &endVelocity,
+		       const unsigned int &polynomialOrder);
  		       
-		/**
-		 * Query the state (position, velocity, acceleration) for the given time.
-		 * @param time The point at which to evaluate the trajectory.
-		 * @return A State data structure.
-		 */
- 		State<DataType> query_state(const DataType &time);
- 		
-	private:
-	
  };                                                                                                 // Semicolon needed after class declaration
  
    //////////////////////////////////////////////////////////////////////////////////////////////////
@@ -56,28 +63,28 @@
  template <class DataType>
  Spline<DataType>::Spline(const std::vector<Eigen::Vector<DataType,Eigen::Dynamic>> &waypoints,
  		          const std::vector<DataType> &times,
- 		          const unsigned int &polynomialOrder,
  		          const Eigen::Vector<DataType,Eigen::Dynamic> &startVelocity,
- 		          const Eigen::Vector<DataType,Eigen::Dynamic> &endVelocity)
+ 		          const Eigen::Vector<DataType,Eigen::Dynamic> &endVelocity,
+ 		          const unsigned int &polynomialOrder)
 {
-	// Set values in underlying Waypoints class
-	this->_numberOfWaypoints = waypoints.size();
-	this->_times = times;
-	
 	using namespace Eigen;
 	
+	// Set values in underlying Waypoints class
+	this->_numberOfWaypoints = waypoints.size();
+	this->_time = times;
+	
 	// Ensure input arguments are sound
-	if(polynomialOrder != 3)
-	{
-		throw std::invalid_argument("[ERROR] [SPLINE] Constructor: "
-		                            "The polynomial order argument was " + std::to_string(polynomialOrder) +
-		                            " but the Spline class can only currently handle 3.");
-	}
-	else if(waypoints.size() != times.size())
+	if(waypoints.size() != times.size())
 	{
 		throw std::invalid_argument("[ERROR] [SPLINE] Constructor: "
 		                            "The number of waypoints does not match the number of times! ("
 		                            + std::to_string(waypoints.size()) + " != " + std::to_string(times.size()) + ".");
+	}
+	else if(waypoints.size() < 2)
+	{
+		throw std::invalid_argument("[ERROR] [SPLINE] Constructor: "
+		                            "A minimum of 2 waypoints is needed to construct a spline, "
+		                            "but you provided " + std::to_string(waypoints.size()) + ".");
 	}
 	
 	// Make sure times are in ascending order
@@ -92,70 +99,86 @@
 			                            + std::to_string(times[i]) + " >= " + std::to_string(times[i+1]) + ".");
 		}
 	}
-	
-	// Compute the velocities for each of the waypoints.
-	// They are governed by the relationship A*xdot = B*x
-	
-	Matrix<DataType,Dynamic,Dynamic> A(this->_numberOfPoints,this->_numberOfPoints);
-	A.setZero();
-	
-	Matrix<DataType,Dynamic,Dynamic> B = A;
-	
-	A(0,0) = 1;
-	for(int i = 1; i < this->_numberOfPoints-1; i++)
-	{
-		DataType dt1 = times[i-1];
-		DataType dt2 = times[i];
 		
-		A(i-1,i) = 1/dt1;
-		A(i,i)   = 2*(1/dt1 + 1/dt2);
-		A(i,i+1) = 1/dt2;
-	
-		B(i-1,i) = -3/(dt1*dt1);
-		B(i,i)   =  3(1/(dt1*dt1) - 1/(dt2*dt2));
-		B(i,i+1) =  3/(dt2*dt2);
-	}
-	A(this->_numberOfPoints, this->_numberOfPoints) = 1;
-	
-	Matrix<DataType,Dynamic,Dynamic> C = A.partialPivLu().solve(B);                             // Makes calcs a little easier
-	
-	std::vector<Vector<DataType,Dynamic>> velocity; velocity.resize(this->_numberOfPoints);     // Array of velocities for all dimensions, waypoints
-
-	for(int i = 0; i < this->_numberOfPoints-1; i++)
+	if(this->_numberOfWaypoints == 2)
 	{
-	
+		this->_trajectory.push_back(Polynomial<DataType>(waypoints.front(),
+		                                                 waypoints.back(),
+		                                                 startVelocity,
+		                                                 endVelocity,
+		                                                 times.front(),
+		                                                 times.back(),
+		                                                 polynomialOrder));
 	}
-/*	
-	for(int i = 0; i < this->_numberOfPoints; i++)
+	else
 	{
-		velocity[i].resize(this->_dimensions);                                              // Resize the Eigen::Vector object within the std::vector object
+		// Solve the velocities for all intermediary waypoints.
+		// For convenience, assume a 3rd order polynomial.
+		// The relationship between velocities and positions is governed by:
+		// A*vel = B*pos ---> vel = A^-1*B*pos
 		
-		for(int j = 0; j < this->_dimensions; j++)
+		Matrix<DataType,Dynamic,Dynamic> A(this->_numberOfWaypoints,this->_numberOfWaypoints);
+		
+		A.setZero();
+		
+		Matrix<DataType,Dynamic,Dynamic> B = A;
+		
+		A(0,0) = 1;
+		
+		for(int i = 1; i < this->_numberOfWaypoints-1; i++)
 		{
-			velocity[i][j] = 0.0;
+			DataType dt1 = times[i-1];
+			DataType dt2 = times[i];
 			
-			// Note: waypoints for a single dimension are stored along the rows,
-			//       so we need to project the rows of the C matrix on to the rows of
-			//       the waypoint array
-			for(int k = 0; k < this->_dimensions; k++) velocity[i][j] += C(j,k)*waypoint[i][k];
+			A(i-1,i) = 1/dt1;
+			A(i,i)   = 2*(1/dt1 + 1/dt2);
+			A(i,i+1) = 1/dt2;
+		
+			B(i-1,i) = -3/(dt1*dt1);
+			B(i,i)   =  3*(1/(dt1*dt1) - 1/(dt2*dt2));
+			B(i,i+1) =  3/(dt2*dt2);
 		}
-	}
+		
+		A(this->_numberOfWaypoints, this->_numberOfWaypoints) = 1;
+		
+		Matrix<DataType,Dynamic,Dynamic> C = A.partialPivLu().solve(B);                             // Makes calcs a little easier
+		
+		std::vector<Vector<DataType,Dynamic>> velocity; velocity.resize(this->_numberOfWaypoints);  // Array of velocities for all dimensions, waypoints
+		
+		for(int i = 0; i < velocity.size(); i++)
+		{
+			velocity[i].resize(waypoints[i].size());                                            // Resize the Eigen::Vector object within the std::vector object
+			
+			for(int j = 0; j < velocity[i].size(); j++)
+			{
+				velocity[i][j] = 0.0;
+				
+				// Note: waypoints for a single dimension are stored along the rows,
+				//       so we need to project the rows of the C matrix on to the rows of
+				//       the waypoint array
+				for(int k = 0; k < velocity.front().size(); k++) velocity[i][j] += C(j,k)*waypoints[i][k];
+			}
+		}
+		
+		std::cout << "\nHere are all the velocity vectors:\n";
+		for(auto &vel : velocity)
+		{
+			std::cout << vel.transpose() << "\n";
+		}
+		std::cout << std::endl;
 
-	// Now create n-1 cubic polynomial splines
-	for(int i = 0; i < this->numPoints-1; i++)
-	{
-		try
+		// Now create n-1 polynomials that define the spline
+		for(int i = 0; i < this->_numberOfWaypoints-1; i++)
 		{
-			this->spline.emplace_back(waypoint[i], waypoint[i+1], velocity[i], velocity[i+1], time[i], time[i+1], 3);
-		}
-		catch(const exception &exception)
-		{
-			std::cerr << exception.what() << std::endl;                                 // Use std::endl instead of "\n"; for std::cerr?
-			
-			throw runtime_error("[ERROR] [CUBIC SPLINE] Constructor: Could not generate the spline.");
+			this->_trajectory.push_back(Polynomial<DataType>(waypoints[i],
+				                                         waypoints[i+1],
+				                                         velocity[i],
+				                                         velocity[i+1],
+				                                         times[i],
+				                                         times[i+1],
+				                                         polynomialOrder));
 		}
 	}
-	*/
 }
 
 #endif
---
+++ Trajectory/test/spline_test.cpp
@@ -0,0 +1,66 @@
+#include <iostream>                                                                                 // std::cerr, std::cout
+#include <Spline.h>                                                                                 // We want to test this
+#include <string>
+ 
+  ////////////////////////////////////////////////////////////////////////////////////////////////////
+ //                                           MAIN                                                 //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char* argv[])
+{
+	if(argc != 2)
+	{
+		std::cerr << "[ERROR] [SPLINE TEST] Incorrect number of arguments. "
+		          << "Usage: './spline_test numberOfPoints'." << std::endl;
+		          
+		return -1;
+	}
+	
+	unsigned int n = std::stoi(argv[1]);
+	
+	// Set up the points for the spline
+	std::vector<Eigen::VectorXf> points;
+	std::vector<float> times;
+	
+	for(int i = 0; i < n; i++)
+	{
+		Eigen::VectorXf temp(1); temp(0) = i;
+		
+		points.push_back(temp);
+		times.push_back(i);
+	}
+	
+	for(auto point : points) std::cout << point << " ";
+	std::cout << "\n";
+	
+	try
+	{
+		Spline<float> trajectory(points, times);
+		
+		float hertz = 20.0;
+		unsigned int steps = 20*(n-1);
+		
+		Eigen::MatrixXf state(steps,3);
+		
+		for(int i = 0; i < steps; i++)
+		{
+			float t = i/hertz;
+			
+			const auto &[pos, vel, acc] = trajectory.query_state(t);
+			
+			state(i,0) = pos(0);
+			state(i,1) = vel(0);
+			state(i,2) = acc(0);
+		}
+		
+		std::cout << "\nHere is the state:\n\n";
+		std::cout << state << std::endl;
+	}
+	catch(const std::exception &exception)
+	{
+		std::cerr << exception.what() << std::endl;
+		
+		return -1;
+	}
+	
+	return 0;
+}
