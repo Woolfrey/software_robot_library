45d99601d5990c0119038a3ae120fb155a7dcd79
Jon <jonathan.woolfrey@gmail.com>
2023-11-28 09:53:50 +0100

    Saving work in progress

--- Trajectory/include/CartesianTrajectory.h
+++ Trajectory/include/CartesianTrajectory.h
@@ -24,7 +24,21 @@ class CartesianTrajectory : public Waypoints<DataType,TrajectoryType>
 {
 	public:
 		/**
-		 * Constructor.
+		 * Constructor for a trajectory with 2 poses.
+		 * @param startPose The initial position & orientation.
+		 * @param endPose The final position & orientation.
+		 * @param startTime The time at which to begin.
+		 * @param endTime Do I really have to explain this?
+		 */
+		CartesianTrajectory(const Pose<DataType> &startPose,
+		                    const Pose<DataType> &endPose,
+		                    const DataType &startTime,
+		                    const DataType &endTime)
+		:
+		CartesianTrajectory(std::vector<Pose<DataType>> {startPose, endPose},
+		                    std::vector<DataType> {startTime, endTime}) {}
+		/**
+		 * Constructor for a trajectory with waypoints.
 		 * @param poses The poses that define the waypoints on the trajectory.
 		 * @param times The time at which to pass through each pose.
 		 */
@@ -61,7 +75,13 @@ template <class DataType, class TrajectoryType>
 CartesianTrajectory<DataType,TrajectoryType>::CartesianTrajectory(const std::vector<Pose<DataType>> &poses,
                                                                   const std::vector<DataType>       &times)
 {
-	if(poses.size() != times.size())
+	if(poses.size() < 2)
+	{
+		throw std::invalid_argument("[ERROR] [CARTESIAN TRAJECTORY] Constructor: "
+		                            "A minimum number of 2 poses is required to construct a "
+		                            "trajectory but you provided " + std::to_string(poses.size()) + ".");
+	}
+	else if(poses.size() != times.size())
 	{
 		throw std::invalid_argument("[ERROR] [CARTESIAN TRAJECTORY] Constructor: "
                                             "Length of input arguments do not match. "
@@ -84,10 +104,12 @@ CartesianTrajectory<DataType,TrajectoryType>::CartesianTrajectory(const std::vec
 		DataType angle = 2*asin(norm);                                                      // vec = (angle/2)*axis; asin = [-pi, pi]
 		
 		if(abs(angle) < 1e-04) point.tail(3) = Eigen::Vector<DataType,3>::Zero();           // Trivially small; zero rotation
-		else                    point.tail(3) = angle*(vec/norm);                           // angle*axis
+		else                   point.tail(3) = angle*(vec/norm);                            // angle*axis
 	
 		points.push_back(point);                                                            // Add to end
 	}
+	
+	this->_trajectory = TrajectoryType(points,times);                                           // Construct the trajectory
 }
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -107,12 +129,13 @@ CartesianState<DataType> CartesianTrajectory<DataType, TrajectoryType>::query_st
 	if(abs(angle) < 1e-04) pose = Pose(position.head(3), Eigen::Quaternion<DataType>(1,0,0,0)); // Assume zero rotation
 	else
 	{
-		Eigen::Vector<DataType,3> axis = position.tail(3).normalized();
+		Eigen::Vector<DataType,3> axis = position.tail(3).normalized();                     // 
 		
-		pose = Pose<DataType>(position.head(3), Eigen::Quaternion<DataType>(cos(0.5*angle),
-		                                                                    sin(0.5*angle)*axis(0),
-		                                                                    sin(0.5*angle)*axis(1),
-		                                                                    sin(0.5*angle)*axis(2)));
+		pose = Pose<DataType>(position.head(3), 
+		                      Eigen::Quaternion<DataType>(cos(0.5*angle),
+		                                                  sin(0.5*angle)*axis(0),
+		                                                  sin(0.5*angle)*axis(1),
+		                                                  sin(0.5*angle)*axis(2)));
 	}
 	
 	CartesianState<DataType> returnValue = {pose, velocity, acceleration};                      // Put them together in data structure
