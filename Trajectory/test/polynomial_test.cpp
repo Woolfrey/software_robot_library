a9e98b8fbe0a8c0bde2c752693a340fe075568e2
Jon <jonathan.woolfrey@gmail.com>
2024-03-17 08:53:45 +0000

    Saving data to csv file for analysis

--- Trajectory/test/polynomial_test.cpp
+++ Trajectory/test/polynomial_test.cpp
@@ -1,3 +1,11 @@
+/**	
+ * @file  : polynomial_test.cpp
+ * @author: Jon Woolfrey
+ * @date  : March 2024
+ * @brief : Saves data on a polynomial trajectory for analysis.
+ */
+ 
+#include <fstream>                                                                                  // For opening/creating/saving text files
 #include <iostream>                                                                                 // std::cerr, std::cout
 #include <Polynomial.h>                                                                             // We want to test this
 #include <string>
@@ -13,7 +21,7 @@ int main(int argc, char* argv[])
 		          << "Usage: './polynomial_test n' where 'n' is an odd integer." << std::endl;
 		          
 	
-		return -1;                                                                           // Failure
+		return -1;                                                                                // Failure
 	}
 	
 	Eigen::Vector<float,1> startPosition; startPosition(0) = 0;
@@ -23,18 +31,19 @@ int main(int argc, char* argv[])
 	
 	try
 	{
-		unsigned int order = std::stoi(argv[1]);
+		unsigned int order = std::stoi(argv[1]);                                                  // Convert to int
 		
-		Polynomial<float> trajectory(startPosition,endPosition,startVelocity,endVelocity,0,1,order);
-	
+		Polynomial<float> trajectory(startPosition, endPosition, startVelocity, endVelocity, 0, 1, order);
+	 
 		float hertz = 50.0;
-		unsigned int steps = 50;
+		
+		unsigned int steps = 51;
 		
 		Eigen::MatrixXf state(steps,3);
 		
 		for(int i = 0; i < steps; i++)
 		{
-			float t = i/hertz;
+			float t = (i-1)/hertz;
 			
 			const auto &[pos, vel, acc] = trajectory.query_state(t);
 			
@@ -43,8 +52,18 @@ int main(int argc, char* argv[])
 			state(i,2) = acc(0);
 		}
 		
-		std::cout << "\nHere is the state:\n\n";
-		std::cout << state << std::endl;
+		// Save the data to .csv file
+	     std::ofstream file;
+	     file.open("polynomial_test_data.csv");
+          
+          for(int i = 0; i < steps; i++)
+          {
+               file << i/hertz;                                                                     // Time
+               for(int j = 0; j < 3; j++) file << "," << state(i,j);                                // Position, velocity, acceleration
+               file << "\n";                                                                        // New line
+          }
+          
+          file.close();                                                                             // As it says on the label
 	}
 	catch(const std::exception &exception)
 	{
