d1befd959c7dff20771ed193e6ce7e6a7e7cd5a2
Jon <jonathan.woolfrey@gmail.com>
2024-04-26 13:38:39 +0100

    Saving work in progress

---
+++ Control/test/kinematic_control_test.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file   kinematic_control_test.cpp
+ * @author Jon Woolfrey
+ * @data   April 2024
+ * @brief  A script for testing the SerialPositionControl class.
+ */
+ 
+#include <SerialKinematicControl.h>
+
+int main(int argc, char** argv)
+{
+     std::cout << "Worker bees can leave.\n";
+     std::cout << "Even drones can fly away.\n";
+     std::cout << "The Queen is their slave.\n";
+     
+     return 0;                                                                                      // No problems with main()
+}
