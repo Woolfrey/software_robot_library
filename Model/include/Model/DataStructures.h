3c74141cc9ce08fcc6aad3cc082956a24b06459d
Jon <jonathan.woolfrey@gmail.com>
2025-05-20 17:06:11 +0100

    New data structures for mobile robots

--- Model/include/Model/DataStructures.h
+++ Model/include/Model/DataStructures.h
@@ -17,6 +17,7 @@
 #define MODEL_DATA_STRUCTS_H
 
 #include <Model/Pose.h>
+#include <Model/Pose2D.h>
 
 namespace RobotLibrary { namespace Model {
 
@@ -56,6 +57,34 @@ struct DifferentialDriveParameters
     Eigen::Matrix3d propagationUncertainty = Eigen::Matrix3d::Identity();                           ///< Uncertainty of configuration propagation in Kalman filter
 
     DifferentialDriveParameters() = default;
+};
+
+/**
+ * @brief A struct that fully defines the state of a differential drive robot.
+ */
+struct DifferentialDriveState
+{
+    Eigen::Matrix3d covariance = Eigen::Matrix3d::Identity();                                       ///< Uncertainty of the pose
+    Eigen::Vector2d velocity = {0.0, 0.0};                                                          ///< Linear & angular velocity, respectively
+    RobotLibrary::Model::Pose2D pose;                                                               ///< Position & orientation relative to a reference frame.
+};
+
+
+/**
+ * @brief A struct that defines an ellipsoid in 2D or 3D space.
+ * 
+ * The ellipsoid is represented as a quadratic form:
+ * (x - centerPoint)^T * shapeMatrix.inverse() * (x - centerPoint) <= 1
+ * 
+ * @tparam Dim The dimension of the space (e.g., 2 or 3)
+ * 
+ * @note shapeMatrix must be symmetric and positive-definite.
+ */
+template <int Dim>
+struct Ellipsoid
+{
+    Eigen::Matrix<double, Dim, Dim> shapeMatrix;                                                    ///< Positive-definite matrix that determines shape
+    Eigen::Vector<double, Dim> centerPoint;                                                         ///< Location of center
 };
 
 } } // namespace
