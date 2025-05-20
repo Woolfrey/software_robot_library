cef66470e02f43bbd5e8a04fecbc6314a0efcb87
Jon <jonathan.woolfrey@gmail.com>
2025-05-20 17:04:04 +0100

    New data struct, renamed old one for clarity

--- Control/include/Control/DataStructures.h
+++ Control/include/Control/DataStructures.h
@@ -25,16 +25,17 @@ namespace RobotLibrary { namespace Control {
 /**
  * @brief A data structure for passing control parameters to the SerialLinkBase class in a single argument.
  */
-struct Parameters
+struct SerialLinkParameters
 {
     // NOTE: These are for the control class itself:
-    Parameters() = default;                                                                         ///< This enables default options
+    SerialLinkParameters() = default;                                                               ///< This enables default options
     
-    double controlFrequency     = 100.0;                                                            ///< Rate at which control loop operates.
     double jointPositionGain    = 100.0;                                                            ///< Scales the position error feedback  
     double jointVelocityGain    = 20.0;                                                             ///< Scales the velocity error feedback
     double maxJointAcceleration = 5.0;                                                              ///< Limits joint acceleration
     double minManipulability    = 1e-04;                                                            ///< Threshold for singularity avoidance
+    
+    unsigned int controlFrequency = 100;                                                            ///< Rate at which control loop operates.    
     
     Eigen::Matrix<double,6,6> cartesianStiffness = (Eigen::MatrixXd(6,6) << 10.0,  0.0,  0.0, 0.0, 0.0, 0.0,
                                                                              0.0, 10.0,  0.0, 0.0, 0.0, 0.0,
@@ -51,6 +52,19 @@ struct Parameters
                                                                           0.0, 0.0, 0.0, 0.0, 0.0, 0.2).finished(); ///< Scales twist error feedback                                                                                                                        
                                                                           
     SolverOptions<double> qpsolver = SolverOptions<double>();                                       ///< Parameters for the underlying QP solver
+};
+
+struct DifferentialDrivePredictiveParameters
+{
+    DifferentialDrivePredictiveParameters() = default;
+  
+    unsigned int numberOfRecursions = 2;
+    
+    unsigned int predictionSteps = 10;
+    
+    Eigen::Matrix3d initialPoseErrorWeight = Eigen::Matrix3d::Identity();
+    
+    Eigen::Matrix3d finalPoseErrorWeight = Eigen::Matrix3d::Identity();
 };
 
 } } // namespace
