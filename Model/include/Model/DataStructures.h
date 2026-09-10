7c842a3774afe1527c1c19bc6702c97a66500c7c
Jon <jonathan.woolfrey@gmail.com>
2026-09-10 19:15:45 +1000

    Removed content for mobile robot control

--- Model/include/Model/DataStructures.h
+++ Model/include/Model/DataStructures.h
@@ -18,7 +18,6 @@
 #define MODEL_DATA_STRUCTS_H
 
 #include <Model/Pose.h>
-#include <Model/Pose2D.h>
 
 namespace RobotLibrary { namespace Model {
 
@@ -41,34 +40,6 @@ struct ReferenceFrame
 {
      Link *link = nullptr;                                                                          ///< The link it is attached to
      Pose relativePose;                                                                             ///< Pose with respect to local link frame
-};
-
-/**
- * @brief A struct for collating parameters for the DifferentialDrive
- */
-struct DifferentialDriveParameters
-{
-    double inertia                = 0.5 * 5.0 * 0.25 * 0.25;                                        ///< Rotational inertia (kg*m^2)
-    double mass                   = 5.0;                                                            ///< Weight (kg)
-    double maxAngularAcceleration = 10.0;                                                           ///< Maximum rotational acceleration (rad/s/s)
-    double maxAngularVelocity     = 100.0 * M_PI / 30.0;                                            ///< Maximum rotational speed (rad/s)
-    double maxLinearAcceleration  = 5.0;                                                            ///< Maximum forward acceleration (m/s/s)
-    double maxLinearVelocity      = 2.0;                                                            ///< Maximum forward speed (m/s)
-    double minimumSafeDistance    = 0.5;                                                            ///< Used for collision detection
-    
-    Eigen::Matrix3d propagationUncertainty = Eigen::Matrix3d::Identity();                           ///< Uncertainty of configuration propagation in Kalman filter
-
-    DifferentialDriveParameters() = default;
-};
-
-/**
- * @brief A struct that fully defines the state of a differential drive robot.
- */
-struct DifferentialDriveState
-{
-    Eigen::Matrix3d covariance = Eigen::Matrix3d::Identity();                                       ///< Uncertainty of the pose
-    Eigen::Vector2d velocity = {0.0, 0.0};                                                          ///< Linear & angular velocity, respectively
-    RobotLibrary::Model::Pose2D pose;                                                               ///< Position & orientation relative to a reference frame.
 };
 
 } } // namespace
